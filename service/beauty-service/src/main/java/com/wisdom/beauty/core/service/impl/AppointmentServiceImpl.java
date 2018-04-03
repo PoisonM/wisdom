ab08ac25662b2f1fd113aaae0b99eb4d55d46e4b
zhaodeliang <zhaodeliang@baoyinxiaofei.com>
2018-04-03 17:11:44 +0800

    商家平台

---
+++ service/beauty-service/src/main/java/com/wisdom/beauty/core/service/impl/AppointmentServiceImpl.java
@@ -0,0 +1,16 @@
+package com.wisdom.beauty.core.service.impl;
+
+import com.wisdom.beauty.core.service.AppointmentService;
+import org.springframework.stereotype.Service;
+
+/**
+ * FileName: AppointmentServiceImpl
+ *
+ * @author: 赵得良
+ * Date:     2018/4/3 0003 15:06
+ * Description: 预约相关
+ */
+@Service
+public class AppointmentServiceImpl implements AppointmentService{
+
+}
