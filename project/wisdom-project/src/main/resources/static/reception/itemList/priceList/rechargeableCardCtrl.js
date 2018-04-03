5055aabff73a8afdbfee1228563c3242540d0327
tangyanchun <tangyanchun@qq.com>
2018-04-03 17:56:39 +0800

    套卡页面 套卡详情页面 充值卡页面 充值卡详情页面

---
+++ project/wisdom-project/src/main/resources/static/reception/itemList/priceList/rechargeableCardCtrl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2018/4/3.
+ */
+PADWeb.controller("rechargeableCardCtrl", function($scope, $state, $stateParams,$rootScope) {
+    console.log("rechargeableCardCtrl");
+    $rootScope.title="充值卡";
+})
