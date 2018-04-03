463dbb7af26264383c948a285d0acba285fb618a
tangyanchun <tangyanchun@qq.com>
2018-04-03 17:57:33 +0800

    项目 页面与js

--- project/wisdom-project/src/main/resources/static/reception/itemList/priceList/projectCtrl.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/priceList/projectCtrl.js
@@ -3,31 +3,85 @@
  */
 PADWeb.controller("projectCtrl", function($scope, $state, $stateParams) {
     $scope.param = {
-        selection:"0"
+        selection:"0",
+        projectAppear:false,
+        parentSty:"",
+        childrenFlag:""
     }
-    $scope.selection  = function (index,$event) {
-        $scope.param.selection = index
-        $event.stopPropagation();
+    // $scope.selectSingleData = [0,1,0,0,0,0,0,0,0,0];
+    $scope.selectSingleData = [
+        {
+            name:"面部",
+            status:"3",
+        },
+        {
+            name:"脚部",
+            status:"1",
+        },
+        {
+            name:"背部",
+            status:"1",
+        },
+        {
+            name:"背部",
+            status:"1",
+        },
+        {
+            name:"背部",
+            status:"1",
+        },
+        {
+            name:"背部",
+            status:"1",
+        },
+        {
+            name:"背部",
+            status:"1",
+        }
+    ]
+    $scope.checkImg = function (index,status) {
+        if($scope.param.childrenFlag == index){
+            for(var i = 0; i < $scope.selectSingleData.length; i++ ){
+                $scope.selectSingleData[i].status = 1
+            }
+            if(status == 1){
+                $scope.selectSingleData[index].status = 4;
+                $scope.param.projectAppear = true;
+            }
+            if(status == 4){
+                $scope.selectSingleData[index].status = 3;
+                $scope.param.projectAppear = false;
+            }
+            if(status == 3){
+                $scope.selectSingleData[index].status = 4;
+                $scope.param.projectAppear = true;
+            }
+        }
     }
-    $scope.selectSingleData = [0,1,0,0,0,0,0,0,0,0];
-    $scope.selectSingleDataPic =[];
+    /*$scope.selectSingleDataPic =[];
     var single =[];
     for(var i=0;i<$scope.selectSingleData.length;i++){
         $scope.selectSingleDataPic[i] = 'images/check2.png';
         single[i] = true;
     }
     $scope.projectAppear=false;
-    $scope.checkImg = function(index,$event) {
+    $scope.checkImg = function(index) {
         if (single[index] == true) {
             $scope.selectSingleDataPic[index] = 'images/check4.png';
             single[index] = false;
            $scope.projectAppear=true;
         } else {
-            $scope.selectSingleDataPic[index] = 'images/check2.png';
+            $scope.selectSingleDataPic[index] = 'images/check3.png';
             single[index] = true;
             $scope.projectAppear=false;
         }
-        $event.stopPropagation();
-
+    }*/
+    $scope.selection  = function (index) {
+        $scope.param.childrenFlag = index
+        for(var i = 0; i < $scope.selectSingleData.length; i++ ){
+            $scope.selectSingleData[i].status = 1
+        }
+        $scope.param.selection = index;
+        $scope.selectSingleData[index].status = 3
     }
 })
