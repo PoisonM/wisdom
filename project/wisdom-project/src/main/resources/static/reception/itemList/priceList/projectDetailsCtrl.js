8b051b62faa4022d70a61950c72cc53d440792de
tangyanchun <tangyanchun@qq.com>
2018-04-03 10:52:01 +0800

    价目表

--- project/wisdom-project/src/main/resources/static/reception/itemList/priceList/projectDetailsCtrl.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/priceList/projectDetailsCtrl.js
@@ -1,8 +1,8 @@
 /**
  * Created by Administrator on 2018/3/30.
  */
-PADWeb.controller("detailsCtrl", function($scope, $state, $stateParams) {
-    console.log("detailsCtrl")
+PADWeb.controller("projectDetailsCtrl", function($scope, $state, $stateParams) {
+    console.log("projectDetailsCtrl")
     $scope.$parent.mainSwitch.footerBoxFlag=false;
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 3,
