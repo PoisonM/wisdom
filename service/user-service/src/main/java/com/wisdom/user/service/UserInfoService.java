569d0e17b8796db48878032fa4e46de3c11d3afb
cjk18 <cus18@sina.com>
2018-04-08 17:17:52 +0800

    wisdom

--- service/user-service/src/main/java/com/wisdom/user/service/UserInfoService.java
+++ service/user-service/src/main/java/com/wisdom/user/service/UserInfoService.java
@@ -1,33 +1,11 @@
 package com.wisdom.user.service;
 
-import com.google.gson.Gson;
-import com.wisdom.common.constant.ConfigConstant;
 import com.wisdom.common.dto.account.PageParamVoDTO;
 import com.wisdom.common.dto.system.PageParamDTO;
 import com.wisdom.common.dto.system.UserBusinessTypeDTO;
 import com.wisdom.common.dto.system.UserInfoDTO;
-import com.wisdom.common.dto.wexin.WeixinShareDTO;
-import com.wisdom.common.dto.wexin.WeixinTokenDTO;
-import com.wisdom.common.util.*;
-import com.wisdom.user.client.BusinessServiceClient;
-import com.wisdom.user.mapper.CustomerInfoMapper;
-import net.sf.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public interface UserInfoService {
 
--- service/user-service/src/main/java/com/wisdom/user/service/impl/LoginServiceImpl.java
+++ service/user-service/src/main/java/com/wisdom/user/service/impl/LoginServiceImpl.java
@@ -3,11 +3,12 @@ package com.wisdom.user.service.impl;
 import com.aliyun.opensearch.sdk.dependencies.com.google.gson.Gson;
 import com.wisdom.common.constant.ConfigConstant;
 import com.wisdom.common.constant.StatusConstant;
+import com.wisdom.common.dto.customer.SysBossDTO;
 import com.wisdom.common.dto.system.LoginDTO;
 import com.wisdom.common.dto.system.UserInfoDTO;
 import com.wisdom.common.dto.system.ValidateCodeDTO;
 import com.wisdom.common.util.*;
-import com.wisdom.user.mapper.CustomerInfoMapper;
+import com.wisdom.user.mapper.UserInfoMapper;
 import com.wisdom.user.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -29,13 +30,14 @@ import java.util.UUID;
 public class LoginServiceImpl implements LoginService{
 
     @Autowired
-    private CustomerInfoMapper userMapper;
+    private UserInfoMapper userMapper;
 
     @Autowired
     protected MongoTemplate mongoTemplate;
 
     private Gson gson = new Gson();
 
+    @Override
     public String userLogin(String phone, String code, String loginIP, String openId) throws Exception {
 
         //判断validateCode是否还有效
@@ -107,7 +109,8 @@ public class LoginServiceImpl implements LoginService{
         JedisUtils.set(logintoken,userInfoStr, ConfigConstant.logintokenPeriod);
         return logintoken;
     }
-    
+
+    @Override
     public String userLoginOut(String logintoken, HttpServletRequest request, HttpServletResponse response, HttpSession session) {
         String openId = WeixinUtil.getUserOpenId(session,request);
         JedisUtils.del(logintoken);
@@ -116,6 +119,7 @@ public class LoginServiceImpl implements LoginService{
         return StatusConstant.LOGIN_OUT;
     }
 
+    @Override
     public String managerLogin(String userPhone, String code) {
         UserInfoDTO userInfoDTO = new UserInfoDTO();
         userInfoDTO.setMobile(userPhone);
@@ -135,9 +139,148 @@ public class LoginServiceImpl implements LoginService{
     }
 
     @Override
-    public String bossLogin(LoginDTO loginDTO, String s, String openid)
+    public String bossMobileLogin(LoginDTO loginDTO, String s, String openId)
     {
-        return null;
+        //判断validateCode是否还有效
+        Query query = new Query().addCriteria(Criteria.where("mobile").is(loginDTO.getUserPhone()))
+                .addCriteria(Criteria.where("code").is(loginDTO.getCode()));
+        query.with(new Sort(new Sort.Order(Sort.Direction.DESC, "createDate")));
+        List<ValidateCodeDTO> data = mongoTemplate.find(query, ValidateCodeDTO.class,"validateCode");
+        if(data==null)
+        {
+            return StatusConstant.VALIDATECODE_ERROR;
+        }
+        else
+        {
+            ValidateCodeDTO validateCodeDTO = data.get(0);
+            Date dateStr = validateCodeDTO.getCreateDate();
+            long period =  (new Date()).getTime() - dateStr.getTime();
+
+            //验证码过了5分钟了
+            if(period>300000)
+            {
+                return  StatusConstant.VALIDATECODE_ERROR;
+            }
+        }
+
+        //validateCode有效后，判断sys_user表中，是否存在此用户，如果存在，则成功返回登录，如果不存在，则创建用户后，返回登录成功
+        SysBossDTO sysBossDTO = new SysBossDTO();
+        userInfoDTO.setUserOpenid(openId);
+        List<UserInfoDTO> userInfoDTOList = userMapper.getUserByInfo(userInfoDTO);
+        RedisLock redisLock = new RedisLock("userInfo"+loginDTO.getUserPhone());
+        try {
+            redisLock.lock();
+
+            if(userInfoDTOList.size()>0)
+            {
+                userInfoDTO = userInfoDTOList.get(0);
+                if(userInfoDTO.getMobile()==null)
+                {
+                    //用户曾经绑定过手机号，更新用户登录信息
+                    userInfoDTO.setMobile(loginDTO.getUserPhone());
+                    userInfoDTO.setLoginDate(new Date());
+                    userInfoDTO.setLoginIp(loginIP);
+                    userMapper.updateUserInfo(userInfoDTO);
+                }
+                else if(userInfoDTO.getMobile().equals(phone))
+                {
+                    userInfoDTO.setLoginDate(new Date());
+                    userInfoDTO.setLoginIp(loginIP);
+                    userMapper.updateUserInfo(userInfoDTO);
+                }
+                else
+                {
+                    return StatusConstant.WEIXIN_ATTENTION_ERROR;
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            redisLock.unlock();
+        }
+
+        userInfoDTO.setNickname(CommonUtils.nameDecoder(userInfoDTO.getNickname()));
+
+        //登录成功后，将用户信息放置到redis中，生成logintoken供前端使用
+        String logintoken = UUID.randomUUID().toString();
+        String userInfoStr = gson.toJson(userInfoDTO);
+        JedisUtils.set(logintoken,userInfoStr, ConfigConstant.logintokenPeriod);
+        return logintoken;
+    }
+
+    @Override
+    public String bossWebLogin(LoginDTO loginDTO, String s)
+    {
+        //判断validateCode是否还有效
+        Query query = new Query().addCriteria(Criteria.where("mobile").is(loginDTO.getUserPhone()))
+                .addCriteria(Criteria.where("code").is(loginDTO.getCode()));
+        query.with(new Sort(new Sort.Order(Sort.Direction.DESC, "createDate")));
+        List<ValidateCodeDTO> data = mongoTemplate.find(query, ValidateCodeDTO.class,"validateCode");
+        if(data==null)
+        {
+            return StatusConstant.VALIDATECODE_ERROR;
+        }
+        else
+        {
+            ValidateCodeDTO validateCodeDTO = data.get(0);
+            Date dateStr = validateCodeDTO.getCreateDate();
+            long period =  (new Date()).getTime() - dateStr.getTime();
+
+            //验证码过了5分钟了
+            if(period>300000)
+            {
+                return  StatusConstant.VALIDATECODE_ERROR;
+            }
+        }
+
+        //validateCode有效后，判断sys_user表中，是否存在此用户，如果存在，则成功返回登录，如果不存在，则创建用户后，返回登录成功
+        UserInfoDTO userInfoDTO = new UserInfoDTO();
+        userInfoDTO.setUserOpenid(openId);
+        //userInfoDTO.setMobile(phone);
+        List<UserInfoDTO> userInfoDTOList = userMapper.getUserByInfo(userInfoDTO);
+        RedisLock redisLock = new RedisLock("userInfo"+phone);
+        try {
+            redisLock.lock();
+
+            if(userInfoDTOList.size()>0)
+            {
+                userInfoDTO = userInfoDTOList.get(0);
+                if(userInfoDTO.getMobile()==null)
+                {
+                    //用户曾经绑定过手机号，更新用户登录信息
+                    userInfoDTO.setMobile(phone);
+                    userInfoDTO.setLoginDate(new Date());
+                    userInfoDTO.setLoginIp(loginIP);
+                    userMapper.updateUserInfo(userInfoDTO);
+                }
+                else if(userInfoDTO.getMobile().equals(phone))
+                {
+                    userInfoDTO.setLoginDate(new Date());
+                    userInfoDTO.setLoginIp(loginIP);
+                    userMapper.updateUserInfo(userInfoDTO);
+                }
+                else
+                {
+                    return StatusConstant.WEIXIN_ATTENTION_ERROR;
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            redisLock.unlock();
+        }
+
+        userInfoDTO.setNickname(CommonUtils.nameDecoder(userInfoDTO.getNickname()));
+
+        //登录成功后，将用户信息放置到redis中，生成logintoken供前端使用
+        String logintoken = UUID.randomUUID().toString();
+        String userInfoStr = gson.toJson(userInfoDTO);
+        JedisUtils.set(logintoken,userInfoStr, ConfigConstant.logintokenPeriod);
+        return logintoken;
     }
 
     @Override
