2f75071c79338578af2388846acdf7460163265e
chenjiake <cjk5510211>
2018-04-13 14:50:14 +0800

    wisdom

--- service/user-service/src/main/java/com/wisdom/user/controller/UserServiceController.java
+++ service/user-service/src/main/java/com/wisdom/user/controller/UserServiceController.java
@@ -16,9 +16,6 @@ public class UserServiceController {
 	@Autowired
 	private UserInfoService customerInfoService;
 
-	@Resource
-	private RealNameAuthService realNameAuthService;
-
 	@RequestMapping(value = "/getUserInfo",method=RequestMethod.POST)
 	@ResponseBody
 	List<UserInfoDTO> getUserInfo(@RequestBody UserInfoDTO userInfoDTO){
@@ -49,18 +46,5 @@ public class UserServiceController {
 	@ResponseBody
 	List<UserInfoDTO> queryNextUserByUserId(@RequestParam(value = "userId") String userId) {
 		return customerInfoService.queryNextUserById(userId);
-	}
-
-	/**
-	 * 用户实名认证接口
-	 *
-	 * @param cardNo
-	 * @param name
-	 * @return
-	 */
-	@RequestMapping(value = "/queryRealNameAuthentication", method = RequestMethod.GET)
-	@ResponseBody
-	RealNameInfoDTO queryRealNameAuthentication(@RequestParam(value = "cardNo") String cardNo, @RequestParam(value = "name") String name) {
-		return realNameAuthService.getRealNameInfoDTO(cardNo, name);
 	}
 }
