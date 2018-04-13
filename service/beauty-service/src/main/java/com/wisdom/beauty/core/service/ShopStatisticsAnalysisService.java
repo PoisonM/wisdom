a69e55203ab05e7396501e3a143c43370c0295cb
zhaodeliang <zhaodeliang@baoyinxiaofei.com>
2018-04-13 09:56:19 +0800

    用户统计分析

---
+++ service/beauty-service/src/main/java/com/wisdom/beauty/core/service/ShopStatisticsAnalysisService.java
@@ -0,0 +1,12 @@
+package com.wisdom.beauty.core.service;
+
+/**
+ * FileName: ShopStatisticsAnalysisService
+ *
+ * @author: 赵得良
+ * Date:     2018/4/3 0003 15:06
+ * Description: 店员相关
+ */
+public interface ShopStatisticsAnalysisService {
+
+}
---
+++ service/beauty-service/src/main/java/com/wisdom/beauty/core/service/impl/ShopStatisticsAnalysisServiceImpl.java
@@ -0,0 +1,16 @@
+package com.wisdom.beauty.core.service.impl;
+
+import com.wisdom.beauty.core.service.ShopStatisticsAnalysisService;
+import org.springframework.stereotype.Service;
+
+/**
+ * FileName: ShopStatisticsAnalysisServiceImpl
+ *
+ * @author: 赵得良
+ * Date:     2018/4/3 0003 15:06
+ * Description: 店员相关
+ */
+@Service("shopStatisticsAnalysisService")
+public class ShopStatisticsAnalysisServiceImpl implements ShopStatisticsAnalysisService {
+
+}
