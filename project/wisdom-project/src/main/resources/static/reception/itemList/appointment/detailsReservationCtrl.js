de8a345cf8af699e578ac5d5301e06b66a8e7ed2
glass <m17610113689@163.com>
2018-04-17 10:53:44 +0800

    新建预约并选择各项

--- project/wisdom-project/src/main/resources/static/reception/itemList/appointment/detailsReservationCtrl.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/appointment/detailsReservationCtrl.js
@@ -1,6 +1,5 @@
 function detailsReservation($scope,ngDialog){
     $scope.goConsumption = function(status){
-        $scope.ngDialog = ngDialog;
         if(status == "去消费"){
             ngDialog.open({
                 template: 'consumption',
@@ -12,7 +11,6 @@ function detailsReservation($scope,ngDialog){
                     ngDialog.close("detailsWrap")
                 }],
                 className: 'ngdialog-theme-default ngdialog-theme-custom',
-
             });
         }else{
             ngDialog.open({
@@ -38,14 +36,11 @@ function detailsReservation($scope,ngDialog){
                                     };
                                 }],
                                 className: 'trueScratchCard ngdialog-theme-custom',
-
                             });
                         }
-
                     };
                 }],
                 className: 'ngdialog-theme-default ngdialog-theme-custom',
-
             });
         }
 
@@ -56,13 +51,19 @@ function detailsReservation($scope,ngDialog){
     };
   /*修改预约*/
     $scope.modifyingAppointment = function(){
-        $scope.param.AppointmentType="长客"
+        $scope.param.AppointmentType="长客";
+        $scope.param.appointmentNew = "no";
+        $scope.param.cancellationFlag = true;
         ngDialog.open({
             template: 'modifyingAppointment',
             scope: $scope, //这样就可以传递参数
             controller: ['$scope', '$interval', function($scope, $interval) {
-                $scope.close = function() {
+                $scope.param.cancellationFlag = true;
+                $scope.close = function(type) {
                     $scope.closeThisDialog();
+                    if(type == 3){
+
+                    }
                 };
             }],
             className: 'modifyingAppointment ngdialog-theme-custom'
@@ -70,11 +71,14 @@ function detailsReservation($scope,ngDialog){
 
     }
     $scope.modifyingAppointmentIndivdual = function(){
+        $scope.param.appointmentNew = "no";
+        $scope.param.cancellationFlag = true;
         ngDialog.open({
             template: 'modifyingAppointment',
             scope: $scope, //这样就可以传递参数
             controller: ['$scope', '$interval', function($scope, $interval) {
-                $scope.close = function() {
+                $scope.param.cancellationFlag = true;
+                $scope.close = function(status) {
                     $scope.closeThisDialog();
                 };
             }],
@@ -82,20 +86,41 @@ function detailsReservation($scope,ngDialog){
         });
         $scope.param.AppointmentType="散客"
     };
+
     modifyingAppointmentPage && modifyingAppointmentPage ($scope,ngDialog);
 
     /*选择顾客*/
-    $scope.selectCustomersCtrl = function(){
+    $scope.openCustomers = function(){
         ngDialog.open({
             template: 'selectCustomersWrap',
             scope: $scope, //这样就可以传递参数
             controller: ['$scope', '$interval', function($scope, $interval) {
                 $scope.close = function() {
+                   /* if(status == 0){
+                        $scope.param.ModifyAppointmentObject.customer =""
+                    }*/
                     $scope.closeThisDialog();
                 };
             }],
             className: 'newProject ngdialog-theme-custom'
         });
+    }
+    $scope.selectCustomersCtrl = function(){
+        $scope.openCustomers()
+
+    };
+    $scope.selectTheCustomer = function(index,item){
+        $scope.param.ModifyAppointmentObject.customerIndex = index;
+        $scope.param.ModifyAppointmentObject.customer = item;
+        if($scope.param.ModifyAppointmentObject.productNum == "0"){
+            setTimeout(function(){
+                $scope.selectNewProduct();
+                ngDialog.close("selectCustomersWrap")
+            },800)
+        }
+
+
+
 
     }
     /*添加顾客*/
@@ -112,56 +137,90 @@ function detailsReservation($scope,ngDialog){
             className: 'newProject ngdialog-theme-custom'
         });
 
-    }
+    };
     /*选择项目*/
     $scope.selectNewProduct = function(){
-        ngDialog.open({
-            template: 'newProduct',
-            scope: $scope, //这样就可以传递参数
-            controller: ['$scope', '$interval', function($scope, $interval) {
-                console.log($scope.$parent.content);
-                $scope.close = function() {
-                    $scope.closeThisDialog();
-                };
-            }],
-            className: 'newProject ngdialog-theme-custom'
-        });
+        if($scope.param.ModifyAppointmentObject.customer == ""){
+            $scope.openCustomers()
+        }else{
+            ngDialog.open({
+                template: 'newProduct',
+                scope: $scope, //这样就可以传递参数
+                controller: ['$scope', '$interval', function($scope, $interval) {
+                    console.log($scope.$parent.content);
+                    $scope.close = function(status) {
+                        if(status == 1){
+                            $scope.param.ModifyAppointmentObject.productData =[];
+                            for(var i=0;i<$scope.param.ModifyAppointmentObject.productIndex.length;i++){
+                                if($scope.param.ModifyAppointmentObject.productIndex[i]==true){
+                                    $scope.param.ModifyAppointmentObject.productData.push($scope.param.ModifyAppointmentObject.productIndex[i])
+                                }
+                            }
+                            $scope.param.ModifyAppointmentObject.productNum = $scope.param.ModifyAppointmentObject.productData.length;
+                            ngDialog.close("selectCustomersWrap")
+                        }else{
+                            $scope.param.ModifyAppointmentObject.productNum = "0";
+                            $scope.falseAll()
+                            $scope.param.ModifyAppointmentObject.productData = []
+
+                        }
+                        $scope.closeThisDialog();
+
+                    };
+                }],
+                className: 'newProject ngdialog-theme-custom'
+            });
+        }
+
 
     }
     /*选择时长*/
     $scope.selectTimeLength = function(){
-        ngDialog.open({
-            template: 'timeLength',
-            scope: $scope, //这样就可以传递参数
-            controller: ['$scope', '$interval', function($scope, $interval) {
-                $scope.param.timeLengthIndex = -1;
-                $scope.param.timeLengthObject.type ='timeLength';
-                $scope.param.timeLengthArr=["1","1.5","2","2.5","3","3.5","4"];
+        if($scope.param.ModifyAppointmentObject.customer == ""){
+            $scope.openCustomers()
+        }else {
+            ngDialog.open({
+                template: 'timeLength',
+                scope: $scope, //这样就可以传递参数
+                controller: ['$scope', '$interval', function ($scope, $interval) {
+                    $scope.param.timeLengthIndex = -1;
+                    $scope.param.ModifyAppointmentObject.type = 'timeLength';
+                    $scope.param.timeLengthArr = ["1", "1.5", "2", "2.5", "3", "3.5", "4"];
 
-                $scope.close = function() {
-                    $scope.closeThisDialog();
-                };
-            }],
-            className: 'timeLength ngdialog-theme-custom'
-        });
+                    $scope.close = function (status) {
+                        if(status == 0){
+                            $scope.param.ModifyAppointmentObject.time =""
+                        }
+                        $scope.closeThisDialog();
+                    };
+                }],
+                className: 'timeLength ngdialog-theme-custom'
+            });
+        }
     };
     /*选择美容师*/
     $scope.selectBeautician = function(){
-        ngDialog.open({
-            template: 'selectBeautician',
-            scope: $scope, //这样就可以传递参数
-            controller: ['$scope', '$interval', function($scope, $interval) {
-                $scope.param.timeLengthIndex = -1;
-                $scope.param.timeLengthObject.type ='beautician';
-                $scope.param.timeLengthArr = ["李","网",'周'];
-                $scope.close = function() {
-                    $scope.closeThisDialog();
-                };
-            }],
-            className: 'selectBeautician ngdialog-theme-custom'
-        });
+        if($scope.param.ModifyAppointmentObject.customer == ""){
+            $scope.openCustomers()
+        }else {
+            ngDialog.open({
+                template: 'selectBeautician',
+                scope: $scope, //这样就可以传递参数
+                controller: ['$scope', '$interval', function ($scope, $interval) {
+                    $scope.param.timeLengthIndex = -1;
+                    $scope.param.ModifyAppointmentObject.type = 'beautician';
+                    $scope.param.timeLengthArr = ["李", "网", '周'];
+                    $scope.close = function (status) {
+                        if(status == 0){
+                            $scope.param.ModifyAppointmentObject.beauticianName =""
+                        }
+                        $scope.closeThisDialog();
+                    };
+                }],
+                className: 'selectBeautician ngdialog-theme-custom'
+            });
+        }
     }
-
     consumption && consumption($scope,ngDialog);
     scratchCard && scratchCard($scope,ngDialog);
 
--- project/wisdom-project/src/main/resources/static/reception/itemList/appointment/newProjectCtrl.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/appointment/newProjectCtrl.js
@@ -1,15 +1,27 @@
 function selectProductCtrl($scope){
 
-    $scope.selectProduct = function(){
-        $scope.param.newProduct = true;
-        $scope.param.modifyingAppointment = false;
-    }
     $scope.newProductBtn = function(index){
         $scope.param.newProductObject.index =index;
         if(index == 1){
             $scope.param.newProductObject.titleFlag = true;
+            $scope.param.newProductObject.content = false;
         }else{
             $scope.param.newProductObject.titleFlag = false;
+            $scope.param.newProductObject.content = true;
+        }
+    };
+    $scope.falseAll = function(){
+        for(var i=0;i<7;i++){
+            $scope.param.ModifyAppointmentObject.productIndex[i] = false;
+        }
+    }
+    $scope.falseAll()
+    $scope.selectTheProduct = function(index,type){
+        if(type == "疗程"){
+            $scope.param.ModifyAppointmentObject.productIndex[index] =!$scope.param.ModifyAppointmentObject.productIndex[index];
+
+        }else{
+            $scope.param.ModifyAppointmentObject.productIndex[index+3] = !$scope.param.ModifyAppointmentObject.productIndex[index+3];
         }
     }
 
