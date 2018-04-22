e46403a9f096b389db2c9a61cb69e1fad0b64897
zhaodeliang <zhaodeliang@baoyinxiaofei.com>
2018-04-22 09:47:50 +0800

    更新预约信息

--- common/src/main/java/com/wisdom/common/util/LunarUtils.java
+++ common/src/main/java/com/wisdom/common/util/LunarUtils.java
@@ -109,15 +109,15 @@ public class LunarUtils {
             e.printStackTrace(); //To change body of catch statement use Options | File Templates.
         }
 
-//求出和1900年1月31日相差的天数
+        //求出和1900年1月31日相差的天数
         int offset = (int) ((cal.getTime().getTime() - baseDate.getTime()) / 86400000L);
         dayCyl = offset + 40;
         monCyl = 14;
 
-//用offset减去每农历年的天数
-// 计算当天是农历第几天
-//i最终结果是农历的年份
-//offset是当年的第几天
+        //用offset减去每农历年的天数
+        // 计算当天是农历第几天
+        //i最终结果是农历的年份
+        //offset是当年的第几天
         int iYear, daysOfYear = 0;
         for (iYear = 1900; iYear < 2050 && offset > 0; iYear++) {
             daysOfYear = yearDays(iYear);
@@ -129,17 +129,17 @@ public class LunarUtils {
             iYear--;
             monCyl -= 12;
         }
-//农历年份
+        //农历年份
         year = iYear;
 
         yearCyl = iYear - 1864;
         leapMonth = leapMonth(iYear); //闰哪个月,1-12
         leap = false;
 
-//用当年的天数offset,逐个减去每月（农历）的天数，求出当天是本月的第几天
+        //用当年的天数offset,逐个减去每月（农历）的天数，求出当天是本月的第几天
         int iMonth, daysOfMonth = 0;
         for (iMonth = 1; iMonth < 13 && offset > 0; iMonth++) {
-//闰月
+            //闰月
             if (leapMonth > 0 && iMonth == (leapMonth + 1) && !leap) {
                 --iMonth;
                 leap = true;
@@ -148,7 +148,7 @@ public class LunarUtils {
                 daysOfMonth = monthDays(year, iMonth);
 
             offset -= daysOfMonth;
-//解除闰月
+            //解除闰月
             if (leap && iMonth == (leapMonth + 1)) leap = false;
             if (!leap) monCyl++;
         }
