52f406e22c6da45137d4afd132e83179fcfd656d
zhaodeliang <1975063953@qq.com>
2018-04-26 13:23:40 +0800

    zhifu

---
+++ service/beauty-service/src/main/java/com/wisdom/beauty/api/extDto/ExtShopProjectInfoDTO.java
@@ -0,0 +1,16 @@
+package com.wisdom.beauty.api.extDto;
+
+import com.wisdom.common.entity.BaseEntity;
+
+import java.io.Serializable;
+
+public class ExtShopProjectInfoDTO extends BaseEntity implements Serializable {
+    //
+    private String id;
+
+    //仓库名称
+    private String name;
+
+    private static final long serialVersionUID = 1L;
+
+}
--- service/beauty-service/src/test/java/com/wisdom/service/impl/ProjectTest.java
+++ service/beauty-service/src/test/java/com/wisdom/service/impl/ProjectTest.java
@@ -1,10 +1,6 @@
 package com.wisdom.service.impl;
 
-import com.alibaba.fastjson.JSONObject;
 import com.wisdom.beauty.BeautyServiceApplication;
-import com.wisdom.beauty.api.dto.ShopUserRechargeCardDTO;
-import com.wisdom.beauty.api.enums.PayTypeEnum;
-import com.wisdom.beauty.api.extDto.ShopUserPayDTO;
 import com.wisdom.common.util.SpringUtil;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,11 +16,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -36,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = BeautyServiceApplication.class)
 @AutoConfigureMockMvc
 @WebAppConfiguration
-public class PayTest {
+public class ProjectTest {
 
     private MockMvc mvc;
 
@@ -44,7 +36,7 @@ public class PayTest {
     private WebApplicationContext context;
 
     @Before
-    public void setupMockMvc() throws Exception {
+    public void setupMockMvc() {
         mvc = MockMvcBuilders.webAppContextSetup(context).build();
         SpringUtil.setApplicationContext(context);
     }
@@ -60,7 +52,7 @@ public class PayTest {
     @Test
     public void getShopUserRecentlyOrderInfo() throws Exception {
 
-        MvcResult result = mvc.perform(get("/projectInfo/getShopCourseProjectList").param("shopUserArchivesId", "1"))
+        MvcResult result = mvc.perform(get("/projectInfo/searchShopProjectList").param("useStyle", "2").param("filterStr", ""))
                 .andExpect(status().isOk())// 模拟向testRest发送get请求
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))// 预期返回值的媒体类型text/plain;charset=UTF-8
                 .andReturn();// 返回执行请求的结果
@@ -68,37 +60,5 @@ public class PayTest {
         System.out.println(result.getResponse().getContentAsString());
     }
 
-    /**
-     * 更新用户订单信息
-     *
-     * @throws Exception
-     */
-    @Test
-    public void userPayOpe() throws Exception {
-
-        ShopUserPayDTO shopUserPayDTO = new ShopUserPayDTO();
-        shopUserPayDTO.setOrderId("20180424200819402");
-        shopUserPayDTO.setPayType(PayTypeEnum.ALI_PAY.getCode());
-        shopUserPayDTO.setShopUserArchivesId("1");
-        shopUserPayDTO.setCashPayPrice("1000");
-        shopUserPayDTO.setSurplusPayPrice("1000");
-        ArrayList<ShopUserRechargeCardDTO> rechargeCardDTOS = new ArrayList<>();
-        ShopUserRechargeCardDTO userRechargeCardDTO = new ShopUserRechargeCardDTO();
-        //消费的金额
-        userRechargeCardDTO.setSurplusAmount(new BigDecimal(100));
-        userRechargeCardDTO.setId("110");
-        rechargeCardDTOS.add(userRechargeCardDTO);
-
-        String toJSONString = JSONObject.toJSONString(shopUserPayDTO);
-
-        System.out.println(toJSONString);
-
-        MvcResult result = mvc.perform(post("/userPay/userPayOpe").contentType(MediaType.APPLICATION_JSON).content(toJSONString))
-                .andExpect(status().isOk())// 模拟向testRest发送get请求
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))// 预期返回值的媒体类型text/plain;charset=UTF-8
-                .andReturn();// 返回执行请求的结果
-
-        System.out.println(result.getResponse().getContentAsString());
-    }
 
 }
