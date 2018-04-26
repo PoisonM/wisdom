bbd0e10848a0559c87cfde52c2a187cff58335dd
tangyanchun <tangyanchun@qq.com>
2018-04-26 19:26:58 +0800

    修改

--- project/wisdom-project/src/main/resources/static/reception/services/padFactory.js
+++ project/wisdom-project/src/main/resources/static/reception/services/padFactory.js
@@ -1,58 +1,51 @@
-var IP = ' http://192.168.1.117/beauty/';
+var IP = 'http://192.168.1.117/';
 var appointmentInfo = "http://localhost:9051/appointmentInfo/";
 PADWeb.factory('httpInterceptor', ["$q", "$injector", function($q) {
         return {
-            request: function(config) {
+            request: function (config) {
                 config.headers = config.headers || {};
                 if (localStorage.getItem("token") != undefined) {
                     config.headers['user_login_token'] = localStorage.getItem("token");
                 }
                 return config || $q.when(config);
             },
-            requestError: function(err) {},
-            response: function(res) {
+            requestError: function (err) {
+            },
+            response: function (res) {
                 return res;
             },
-            responseError: function(err) {}
+            responseError: function (err) {
+            }
         };
     }])
     /**
      * 
      */
     //
-    .factory('ShopDayAppointmentInfoByDate', ['$resource', function($resource) {
+    .factory('DriverReservationList', ['$resource', function ($resource) {
+        return $resource(IP + 'list')
+    }])
+    .factory('ShopDayAppointmentInfoByDate', ['$resource', function ($resource) {
         return $resource(appointmentInfo + 'shopDayAppointmentInfoByDate')
     }])
     .factory('Archives', ['$resource', function($resource) {
-        return $resource(IP + 'archives/:userId', { userId: '@id' })
+        return $resource(IP + 'beauty/archives/:userId', { userId: '@id' })
     }])
-    //待领取汇总
-    .factory('GetProductRecord', ['$resource', function($resource) {
-        return $resource(IP + 'mine/getProductRecord')
+    //项目的接口
+    .factory('OneLevelProject', ['$resource', function($resource) {
+        return $resource(IP+ 'beauty/projectInfo/oneLevelProject')
     }])
-    //店员相关的记录统计
-    .factory('Consumes', ['$resource', function($resource) {
-        return $resource(IP + 'mine/consumes')
+    .factory('TwoLevelProject', ['$resource', function($resource) {
+        return $resource(IP+ 'beauty/projectInfo/twoLevelProject')
     }])
-    //获取档案列表
-    .factory('FindArchives', ['$resource', function($resource) {
-        return $resource(IP + 'archives/findArchives')
+    .factory('ThreeLevelProject', ['$resource', function($resource) {
+        return $resource(IP+ 'beauty/projectInfo/threeLevelProject')
     }])
-    //添加档案
-    .factory('SaveArchiveInfo', ['$resource', function($resource) {
-        return $resource(IP + 'archives/saveArchiveInfo')
-    }])
-    //删除档案信息
-    .factory('DeleteArchiveInfo', ['$resource', function($resource) {
-        return $resource(IP + 'archives/deleteArchiveInfo')
-    }])
-    //查询某用户档案信息
-    .factory('GetShopUserArchivesInfoByUserId', ['$resource', function($resource) {
-        return $resource(IP + 'archives/getShopUserArchivesInfoByUserId')
+   /*产品接口*/
+    .factory('OneLevelProduct', ['$resource', function($resource) {
+        return $resource(IP+ 'beauty/productInfo/oneLevelProduct')
     }])
 
 
 
-
-
-;
+     ;
