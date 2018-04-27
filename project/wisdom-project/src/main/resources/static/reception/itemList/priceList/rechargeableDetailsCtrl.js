567fc440bd8da10d01f83e49fb0a4efeb3ad7d97
tangyanchun <tangyanchun@qq.com>
2018-04-27 19:56:24 +0800

    价目表

--- project/wisdom-project/src/main/resources/static/reception/itemList/priceList/rechargeableDetailsCtrl.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/priceList/rechargeableDetailsCtrl.js
@@ -1,9 +1,9 @@
 /**
  * Created by Administrator on 2018/4/3.
  */
-PADWeb.controller("rechargeableDetailsCtrl", function($scope, $state, $stateParams,$rootScope) {
+PADWeb.controller("rechargeableDetailsCtrl", function($scope, $state, $stateParams,$rootScope,GetCardInfo) {
     /*-------------------------------------------定义头部/左边信息--------------------------------*/
-    $scope.$parent.param.headerPrice.title = "套卡详情"
+    $scope.$parent.param.headerPrice.title = "充值卡详情"
     $scope.flagFn = function (bool) {
         //头
         $scope.$parent.mainSwitch.headerReservationAllFlag = !bool
@@ -16,7 +16,6 @@ PADWeb.controller("rechargeableDetailsCtrl", function($scope, $state, $statePara
     /*打开收银头部/档案头部/我的头部*/
     $scope.flagFn(true)
     console.log("rechargeableDetailsCtrl");
-    $rootScope.title="充值卡详情";
     $scope.$parent.mainSwitch.footerBoxFlag=false;
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 3,
@@ -28,4 +27,16 @@ PADWeb.controller("rechargeableDetailsCtrl", function($scope, $state, $statePara
             clickable: true,
         },
     });
+
+    $scope.param={
+        rechargeList:{},
+        urlArray:{}
+    }
+    GetCardInfo.get({id:"2"},function (data) {
+        $scope.rechargeList=data.responseData;
+        //将轮播图数据截取
+        /*var url= $scope.rechargeList.imageUrl;
+        $scope.param.urlArray = url.split(",");*/
+        console.log(data)
+    })
 })
--- project/wisdom-project/src/main/resources/static/reception/services/padFactory.js
+++ project/wisdom-project/src/main/resources/static/reception/services/padFactory.js
@@ -17,7 +17,7 @@ PADWeb.factory('httpInterceptor', ["$q", "$injector", function($q) {
         };
     }])
     /**
-     *
+     * 
      */
     //
     .factory('ShopDayAppointmentInfoByDate', ['$resource', function($resource) {
@@ -30,11 +30,7 @@ PADWeb.factory('httpInterceptor', ["$q", "$injector", function($q) {
     .factory('GetProductRecord', ['$resource', function($resource) {
         return $resource(IP + 'mine/getProductRecord')
     }])
-    //领取记录
-    .factory('GetWaitReceiveDetail', ['$resource', function($resource) {
-        return $resource(IP + 'mine/getWaitReceiveDetail')
-    }])
-    //查询店员相关的记录统计
+    //店员相关的记录统计
     .factory('Consumes', ['$resource', function($resource) {
         return $resource(IP + 'mine/consumes')
     }])
@@ -54,22 +50,55 @@ PADWeb.factory('httpInterceptor', ["$q", "$injector", function($q) {
     .factory('GetShopUserArchivesInfoByUserId', ['$resource', function($resource) {
         return $resource(IP + 'archives/getShopUserArchivesInfoByUserId')
     }])
-    //
-    .factory('GetClerkAchievement', ['$resource', function($resource) {
-        return $resource(IP + 'work/getClerkAchievement')
+    //项目的接口
+    .factory('OneLevelProject', ['$resource', function($resource) {
+        return $resource(IP+ 'projectInfo/oneLevelProject')
     }])
-    //日预约请求接口
-    .factory('ShopDayAppointmentInfoByDate', ['$resource', function($resource) {
-        return $resource(IP + 'appointmentInfo/shopDayAppointmentInfoByDate')
+    .factory('TwoLevelProject', ['$resource', function($resource) {
+        return $resource(IP+ 'projectInfo/twoLevelProject')
     }])
-    //根据预约主键查询预约项目
-    .factory('GetUserCardProjectList', ['$resource', function($resource) {
-        return $resource(IP + 'projectInfo/getUserCardProjectList',{ appointmentId: '@id' })
+    .factory('ThreeLevelProject', ['$resource', function($resource) {
+        return $resource(IP+ 'projectInfo/threeLevelProject')
     }])
-    //根据预约主键获取预约详情
-    .factory('GetAppointmentInfoById', ['$resource', function($resource) {
-        return $resource(IP + 'appointmentInfo/getAppointmentInfoById',{ appointmentId: '@id' })
+    //项目的详情页面
+    .factory('ProjectInfo', ['$resource', function($resource) {
+        return $resource(IP+ 'projectInfo/:id',{id:'@id'})
     }])
+
+    /*产品接口*/
+    .factory('OneLevelProduct', ['$resource', function($resource) {
+        return $resource(IP+ 'productInfo/oneLevelProduct')
+    }])
+    .factory('TwoLevelProduct', ['$resource', function($resource) {
+        return $resource(IP+ 'productInfo/twoLevelProduct')
+    }])
+    .factory('ThreeLevelProduct', ['$resource', function($resource) {
+        return $resource(IP+ 'productInfo/threeLevelProduct')
+    }])
+    .factory('ProductInfo', ['$resource', function($resource) {
+        return $resource(IP+ 'productInfo/:productId',{productId:'@productId'})
+    }])
+
+    /*套卡接口*/
+    .factory('GetShopProjectGroups', ['$resource', function($resource) {
+        return $resource(IP+ 'cardInfo/getShopProjectGroups')
+    }])
+    .factory('Detail', ['$resource', function($resource) {
+        return $resource(IP+ 'cardInfo/getShopProjectGroup/detail')
+    }])
+    /*充值卡接口*/
+    .factory('GetRechargeCardList', ['$resource', function($resource) {
+        return $resource(IP+ 'cardInfo/getRechargeCardList')
+    }])
+    .factory('GetCardInfo', ['$resource', function($resource) {
+        return $resource(IP+ 'cardInfo/:id', { id: '@id' })
+    }])
+
+
+
+
+
+
 
 
 
