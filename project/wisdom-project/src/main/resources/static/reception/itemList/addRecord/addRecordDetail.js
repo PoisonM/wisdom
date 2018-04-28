68801e99142cd7e3966aff335cea286bab75dc15
taoshiwen <624301656@qq.com>
2018-04-28 18:58:56 +0800

    档案详情

---
+++ project/wisdom-project/src/main/resources/static/reception/itemList/addRecord/addRecordDetail.js
@@ -0,0 +1,33 @@
+PADWeb.controller('addRecordDetailCtrl', function($scope) {
+    console.log($scope);
+    /*-------------------------------------------定义头部/左边信息--------------------------------*/
+    $scope.$parent.$parent.param.headerCash.leftContent="档案(9010)"
+    $scope.$parent.$parent.param.headerCash.leftAddContent="添加档案"
+    $scope.$parent.$parent.param.headerCash.backContent="返回"
+    $scope.$parent.$parent.param.headerCash.leftTip=""
+    $scope.$parent.$parent.mainSwitch.headerCashFlag.headerCashRightFlag.leftFlag = true
+    $scope.$parent.$parent.mainSwitch.headerCashFlag.headerCashRightFlag.middleFlag = true
+    $scope.$parent.$parent.mainSwitch.headerCashFlag.headerCashRightFlag.rightFlag = false
+    $scope.$parent.$parent.param.headerCash.title = "档案详情"
+    $scope.flagFn = function (bool) {
+        //左
+        $scope.$parent.mainLeftSwitch.peopleListFlag = bool
+        $scope.$parent.mainLeftSwitch.priceListFlag = !bool
+        //头
+        $scope.$parent.$parent.mainSwitch.headerReservationAllFlag = !bool
+        $scope.$parent.$parent.mainSwitch.headerCashAllFlag = bool
+        $scope.$parent.$parent.mainSwitch.headerPriceListAllFlag = !bool
+        $scope.$parent.$parent.mainSwitch.headerLoginFlag = !bool
+        $scope.$parent.$parent.mainSwitch.headerCashFlag.leftFlag = bool
+        $scope.$parent.$parent.mainSwitch.headerCashFlag.middleFlag = bool
+        $scope.$parent.$parent.mainSwitch.headerCashFlag.rightFlag = bool
+    }
+    /*打开收银头部/档案头部/我的头部*/
+    $scope.flagFn(true)
+
+
+    /*----------------------------------初始化参数------------------------------------------*/
+
+    /*---------------------------------方法-----------------------------------*/
+
+});
