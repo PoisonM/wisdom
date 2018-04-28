ee5ec2fdb7899fd5d5b8f672fc754ed821f9122f
meixiaopeng <94948521@163.com>
2018-04-28 14:28:00 +0800

    例行提交

--- project/wisdom-project/src/main/resources/static/reception/services/padFactory.js
+++ project/wisdom-project/src/main/resources/static/reception/services/padFactory.js
@@ -68,55 +68,55 @@ PADWeb.factory('httpInterceptor', ["$q", "$injector", function($q) {
     }])
     //根据预约主键查询预约项目
     .factory('GetUserCardProjectList', ['$resource', function($resource) {
-        return $resource(IP + 'projectInfo/getUserCardProjectList',{ appointmentId: '@id' })
+        return $resource(IP + 'projectInfo/getUserCardProjectList', { appointmentId: '@id' })
     }])
     //根据预约主键获取预约详情
     .factory('GetAppointmentInfoById', ['$resource', function($resource) {
-        return $resource(IP + 'appointmentInfo/getAppointmentInfoById',{ appointmentId: '@id' })
+        return $resource(IP + 'appointmentInfo/getAppointmentInfoById', { appointmentId: '@id' })
     }])
 
     //项目的接口
     .factory('OneLevelProject', ['$resource', function($resource) {
-        return $resource(IP+ 'projectInfo/oneLevelProject')
+        return $resource(IP + 'projectInfo/oneLevelProject')
     }])
     .factory('TwoLevelProject', ['$resource', function($resource) {
-        return $resource(IP+ 'projectInfo/twoLevelProject')
+        return $resource(IP + 'projectInfo/twoLevelProject')
     }])
     .factory('ThreeLevelProject', ['$resource', function($resource) {
-        return $resource(IP+ 'projectInfo/threeLevelProject')
+        return $resource(IP + 'projectInfo/threeLevelProject')
     }])
     //项目的详情页面
     .factory('ProjectInfo', ['$resource', function($resource) {
-        return $resource(IP+ 'projectInfo/:id',{id:'@id'})
+        return $resource(IP + 'projectInfo/:id', { id: '@id' })
     }])
 
     /*产品接口*/
     .factory('OneLevelProduct', ['$resource', function($resource) {
-        return $resource(IP+ 'productInfo/oneLevelProduct')
+        return $resource(IP + 'productInfo/oneLevelProduct')
     }])
     .factory('TwoLevelProduct', ['$resource', function($resource) {
-        return $resource(IP+ 'productInfo/twoLevelProduct')
+        return $resource(IP + 'productInfo/twoLevelProduct')
     }])
     .factory('ThreeLevelProduct', ['$resource', function($resource) {
-        return $resource(IP+ 'productInfo/threeLevelProduct')
+        return $resource(IP + 'productInfo/threeLevelProduct')
     }])
     .factory('ProductInfo', ['$resource', function($resource) {
-        return $resource(IP+ 'productInfo/:productId',{productId:'@productId'})
+        return $resource(IP + 'productInfo/:productId', { productId: '@productId' })
     }])
 
     /*套卡接口*/
     .factory('GetShopProjectGroups', ['$resource', function($resource) {
-        return $resource(IP+ 'cardInfo/getShopProjectGroups')
+        return $resource(IP + 'cardInfo/getShopProjectGroups')
     }])
     .factory('Detail', ['$resource', function($resource) {
-        return $resource(IP+ 'cardInfo/getShopProjectGroup/detail')
+        return $resource(IP + 'cardInfo/getShopProjectGroup/detail')
     }])
     /*充值卡接口*/
     .factory('GetRechargeCardList', ['$resource', function($resource) {
-        return $resource(IP+ 'cardInfo/getRechargeCardList')
+        return $resource(IP + 'cardInfo/getRechargeCardList')
     }])
     .factory('GetCardInfo', ['$resource', function($resource) {
-        return $resource(IP+ 'cardInfo/:id', { id: '@id' })
+        return $resource(IP + 'cardInfo/:id', { id: '@id' })
     }])
 
 
