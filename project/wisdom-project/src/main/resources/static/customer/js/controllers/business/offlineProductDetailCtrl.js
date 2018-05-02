56a996ce3b5722ea0c694977fd14f85f3a613200
zhangchao <347280977@qq.com>
2018-05-02 12:00:19 +0800

    点击购物车提示登录

--- project/wisdom-project/src/main/resources/static/customer/js/controllers/business/offlineProductDetailCtrl.js
+++ project/wisdom-project/src/main/resources/static/customer/js/controllers/business/offlineProductDetailCtrl.js
@@ -173,18 +173,7 @@ angular.module('controllers',[]).controller('offlineProductDetailCtrl',
                 }, 1000);
             };
 
-           $scope.loginCart = function(){
-                   IsLogin.save(function(data){
-                        if(data.responseData=="failure"){
-                            showToast("请先登录账号");
-                            hideToast();
-                            $state.go("login");
-                        }else{
-                            $state.go("buyCart");
-                        }
 
-                   })
-           };
 
             $scope.$on('$ionicView.enter', function(){
                 $scope.param = {
