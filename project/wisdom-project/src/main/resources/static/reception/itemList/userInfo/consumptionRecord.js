bd3ce4afb0ed074757eec4b9c8478a4a045875f5
taoshiwen <624301656@qq.com>
2018-05-04 15:41:10 +0800

    接口调试

--- project/wisdom-project/src/main/resources/static/reception/itemList/userInfo/consumptionRecord.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/userInfo/consumptionRecord.js
@@ -1,6 +1,10 @@
-PADWeb.controller('consumptionRecordCtrl', function($scope, $stateParams,Consumes) {
+PADWeb.controller('consumptionRecordCtrl', function($scope, $state,$stateParams,Consumes) {
     /*-------------------------------------------定义头部信息----------------------------------------------*/
-    $scope.$parent.param.headerCash.title="消费记录"
+    $scope.$parent.$parent.param.headerCash.title="消费记录";
+    $scope.$parent.$parent.param.headerCash.backContent = "今日收银记录"
+    $scope.$parent.$parent.mainSwitch.headerCashFlag.headerCashRightFlag.leftFlag = true
+
+
     console.log("消费记录");
     //consumeType 0：充值 1：消费 2、还欠款 3、退款
     Consumes.save({
@@ -11,4 +15,9 @@ PADWeb.controller('consumptionRecordCtrl', function($scope, $stateParams,Consume
     },function (data) {
         $scope.dataList = data.responseData
     })
+    
+    
+    /*$scope.$parent.$parent.backHeaderCashFn = function () {
+        $state.go("pad-web.userInfo.todayPerformance")
+    }*/
 });
