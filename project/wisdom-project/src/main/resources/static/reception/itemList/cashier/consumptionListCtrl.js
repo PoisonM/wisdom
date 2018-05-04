2e03376a19803e8b97e2332e7423deb88bf440dc
meixiaopeng <94948521@163.com>
2018-05-04 15:54:29 +0800

    消费部分提交

--- project/wisdom-project/src/main/resources/static/reception/itemList/cashier/consumptionListCtrl.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/cashier/consumptionListCtrl.js
@@ -1,4 +1,4 @@
-PADWeb.controller('consumptionListCtrl', function($scope, $stateParams, ngDialog, Archives) {
+PADWeb.controller('consumptionListCtrl', function($scope, $stateParams, ngDialog, Archives, SearchShopProjectList, SearchShopProductList, GetShopProjectGroups, ThreeLevelProject, productInfoThreeLevelProject) {
     /*-------------------------------------------定义头部/左边信息--------------------------------*/
     $scope.$parent.param.headerCash.leftContent = "档案(9010)";
     $scope.$parent.param.headerCash.leftAddContent = "添加档案";
@@ -20,6 +20,84 @@ PADWeb.controller('consumptionListCtrl', function($scope, $stateParams, ngDialog
     /*打开收银头部/档案头部/我的头部*/
     $scope.select = 0;
     $scope.tabclick = function(e) {
+        if (e == 3) {
+            $scope.consumptionListUlShow = false;
+            $scope.pl100 = '{"padding-left": "100px"}';
+        } else {
+            $scope.consumptionListUlShow = true;
+            $scope.pl100 = '{}'
+        }
         $scope.select = e;
+        if (e == 0 || e == 1) {
+            SearchShopProjectList.get({
+                filterStr: '',
+                useStyle: e,
+            }, function(data) {
+                $scope.secondCategory = data.responseData.detailLevel;
+                var first = data.responseData.detailLevel[0];
+                var firstkey = '',
+                    secondkey = '';
+                for (var key in first) {
+                    firstkey = key;
+                    break
+                }
+                var second = first[firstkey];
+                for (var key in second) {
+                    secondkey = key;
+                    break
+                }
+                $scope.getThreeCategories(second[secondkey].projectTypeOneId, second[secondkey].projectTypeTwoId);
+            })
+        } else if (e == 2) {
+            SearchShopProductList.get({
+                pageSize: "100",
+                filterStr: '',
+            }, function(data) {
+                $scope.secondCategory = data.responseData.detailLevel;
+                var first = data.responseData.detailLevel[0];
+                var firstkey = '',
+                    secondkey = '';
+                for (var key in first) {
+                    firstkey = key;
+                    break
+                }
+                var second = first[firstkey];
+                for (var key in second) {
+                    secondkey = key;
+                    break
+                }
+                $scope.getProductInfoThreeLevelProject(second[secondkey].productTypeOneId, second[secondkey].productTypeTwoId);
+            })
+        } else if (e == 3) {
+            GetShopProjectGroups.get({
+                projectGroupName: '',
+                pageSize: 100
+            }, function(data) {
+                $scope.threeCategories = data.responseData;
+            })
+        }
+    }
+
+    $scope.tabclick(0);
+    $scope.getThreeCategories = function(one, two) {
+        ThreeLevelProject.get({
+            pageSize: 100,
+            projectName: "",
+            projectTypeOneId: one,
+            ProjectTypeTwoId: two,
+        }, function(data) {
+            $scope.threeCategories = data.responseData;
+        });
+    }
+    $scope.getProductInfoThreeLevelProject = function(one, two) {
+        productInfoThreeLevelProject.get({
+            pageSize: 100,
+            productName: "",
+            productTypeOneId: one,
+            productTypeTwoId: two,
+        }, function(data) {
+            $scope.threeCategories = data.responseData;
+        });
+
     }
 });
