3d88a39aa315d8268cab70997c7dad9e8fb5b0a1
zhangchao <347280977@qq.com>
2018-05-04 15:26:08 +0800

    增加建议不能为空的校验

--- project/wisdom-project/src/main/resources/static/management/js/controllers/withdrawCtrl.js
+++ project/wisdom-project/src/main/resources/static/management/js/controllers/withdrawCtrl.js
@@ -7,6 +7,7 @@ angular.module('controllers',[]).controller('withdrawCtrl',
             var updateEndTime = document.querySelector(".updateEndTime");
             $scope.counnt ="";
             $scope.mum = true;
+            $scope.isDisabled = false;
             var pattern = /^1[34578]\d{9}$/;
             var pattern1 = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
 /*日期插件*/
@@ -134,6 +135,7 @@ angular.module('controllers',[]).controller('withdrawCtrl',
                     $scope.moneyAmount = moneyAmount;
             };
             $scope.changeWithdraw = function(status){
+                $scope.isDisabled = true;
                 var  withDrawRecordDTO={
                         withdrawId:$scope.id,
                         status:status,
