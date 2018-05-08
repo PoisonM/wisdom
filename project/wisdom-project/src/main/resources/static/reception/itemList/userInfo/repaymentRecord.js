6c8ef04a4dec3d17501f151c8662a45f617f561d
taoshiwen <624301656@qq.com>
2018-05-08 18:50:35 +0800

    接口调试

--- project/wisdom-project/src/main/resources/static/reception/itemList/userInfo/repaymentRecord.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/userInfo/repaymentRecord.js
@@ -1,6 +1,6 @@
 PADWeb.controller('repaymentRecordCtrl', function($scope, $stateParams, ngDialog,Consumes) {
     /*-------------------------------------------定义头部信息----------------------------------------------*/
-    $scope.$parent.param.headerCash.title="还欠款记录"
+    $scope.$parent.$parent.param.headerCash.title="还欠款记录"
     $scope.$parent.$parent.param.headerCash.backContent = "今日收银记录"
     $scope.$parent.$parent.mainSwitch.headerCashFlag.headerCashRightFlag.leftFlag = true
     console.log('还款记录');
