8ba59163b158d1614258544cd7247812c36068b4
cjk18 <cus18@sina.com>
2018-05-08 09:59:03 +0800

    wisdom

--- project/wisdom-project/src/main/resources/static/boss/js/services/bossFactory.js
+++ project/wisdom-project/src/main/resources/static/boss/js/services/bossFactory.js
@@ -1,6 +1,7 @@
 
 var user = '/user/';
-var beautyIP = ' http://192.168.1.117/beauty/';
+var appointmentInfo = '/beauty/appointmentInfo/';
+var work = '/beauty/work/'
 
 define(['appBoss'], function (app) {
     app
@@ -10,22 +11,24 @@ define(['appBoss'], function (app) {
         }])
 /*根据时间查询各店预约情况*/
         .factory('GetShopAppointmentNumberInfo',['$resource',function ($resource){
-            return $resource(beautyIP + 'appointmentInfo/getShopAppointmentNumberInfo')
+            return $resource(appointmentInfo + 'getShopAppointmentNumberInfo')
         }])
         /*根据时间查询某个店的预约详情*/
         .factory('ShopDayAppointmentInfoByDate',['$resource',function ($resource){
-            return $resource(beautyIP + 'appointmentInfo/shopDayAppointmentInfoByDate')
+            return $resource(appointmentInfo + 'shopDayAppointmentInfoByDate')
         }])
     /*获取某个老板下面的某个店下的某个美容师的预约列表*/
         .factory('GetShopClerkAppointmentInfo',['$resource',function ($resource){
-        return $resource(beautyIP + 'appointmentInfo/getShopClerkAppointmentInfo')
+        return $resource(appointmentInfo + 'getShopClerkAppointmentInfo')
         }])
 /*根据预约状态获取某个老板下面的某个店的预约列表*/
         .factory('GetShopAppointmentInfoByStatus',['$resource',function ($resource){
-            return $resource(beautyIP + 'appointmentInfo/getShopAppointmentInfoByStatus')
+            return $resource(appointmentInfo + 'getShopAppointmentInfoByStatus')
        }])
 
-
+        .factory('GetExpenditureAndIncome',['$resource',function ($resource){
+            return $resource(beautyIP + 'work/getExpenditureAndIncome')
+        }])
 
 
 });
