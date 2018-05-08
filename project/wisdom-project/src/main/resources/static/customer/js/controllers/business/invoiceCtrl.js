c797df47c9c35b1007c7ec28d061bc09606c8889
zhangchao <347280977@qq.com>
2018-05-08 19:07:04 +0800

    增加建议不能为空的校验

--- project/wisdom-project/src/main/resources/static/customer/js/controllers/business/invoiceCtrl.js
+++ project/wisdom-project/src/main/resources/static/customer/js/controllers/business/invoiceCtrl.js
@@ -38,7 +38,7 @@ angular.module('controllers',[]).controller('invoiceCtrl',
                 if(value=="1"){
                     if($scope.param.companyName==""||$scope.param.taxpayerNumber==""){
                         var alertPopup = $ionicPopup.alert({
-                            template: '<span style="font-size: 0.2rem;color: #333333;margin-left: 0.6rem">请填写正确的公司名称及纳税号</span>',
+                            template: '<span style="font-size: 0.3rem;color: #333333;margin-left: 0.25rem">请填写正确的公司名称及纳税号</span>',
                             okText:'确定'
                         });
                         return;
