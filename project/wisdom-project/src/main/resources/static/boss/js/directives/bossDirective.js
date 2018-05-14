815a6e590feef62509857d8dad9227bc1cf257c6
201 <790785836@qq.com>
2018-05-14 11:39:26 +0800

    beauty

--- project/wisdom-project/src/main/resources/static/boss/js/directives/bossDirective.js
+++ project/wisdom-project/src/main/resources/static/boss/js/directives/bossDirective.js
@@ -7,11 +7,11 @@ define(['appBoss','jquery'], function (app,$) {
                     replace: true,
                     template: '<ion-tabs class="tabs-icon-top tabs-color-active-positive">' +
                     '<ion-tab title="工作" icon-off="ion-ios-personadd" icon-on="ion-ios-personadd" ui-sref="workHome">' +
-                    '<ion-nav-view name="tab-account"></ion-nav-view></ion-tab>' +
+                    '</ion-tab>' +
                     '<ion-tab title="档案" icon-off="ion-ios-filing" icon-on="ion-ios-filing" ui-sref="partialFiles">' +
-                    '<ion-nav-view name="tab-account"></ion-nav-view></ion-tab>' +
+                    '</ion-tab>' +
                     '<ion-tab title="我的" icon-off="ion-android-contact" icon-on="ion-android-contact" ui-sref="myself">' +
-                    '<ion-nav-view name="tab-dash"></ion-nav-view></ion-tab></ion-tabs>',
+                    '</ion-tabs>',
                     link: function(scope,ele,attrs) {
                     }
                 }
