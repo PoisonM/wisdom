74723d3a5854b5a0ac27d1f803b0a40817ac5d2e
tangyanchun <tangyanchun@qq.com>
2018-05-14 16:59:28 +0800

    添加 收支分析的某个店页面

---
+++ project/wisdom-project/src/main/resources/static/boss/js/controllers/work/oneIncomeAnalysisCtrl.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2018/5/14.
+ */
+angular.module('controllers',[]).controller('oneIncomeAnalysisCtrl',
+    ['$scope','$rootScope','$stateParams','$state',
+        function ($scope,$rootScope,$stateParams,$state) {
+
+            $rootScope.title = "";
+
+        }]);
