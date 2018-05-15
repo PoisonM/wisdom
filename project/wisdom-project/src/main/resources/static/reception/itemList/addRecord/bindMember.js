60c5dbea2651db1f97bec7c21f6ccb594fa99aaa
taoshiwen <624301656@qq.com>
2018-05-15 17:09:21 +0800

    接口调试

--- project/wisdom-project/src/main/resources/static/reception/itemList/addRecord/bindMember.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/addRecord/bindMember.js
@@ -1,4 +1,4 @@
-PADWeb.controller('bindMemberCtrl', function ($scope, $state, getBeautyQRCode, getUserScanInfo) {
+PADWeb.controller('bindMemberCtrl', function ($scope,$stateParams, $state, getBeautyQRCode, getUserScanInfo) {
     console.log($scope);
     /*-------------------------------------------定义头部/左边信息--------------------------------*/
     $scope.$parent.mainSwitch.headerPriceListAllFlag = true
@@ -8,16 +8,16 @@ PADWeb.controller('bindMemberCtrl', function ($scope, $state, getBeautyQRCode, g
     $scope.$parent.mainSwitch.footerBoxFlag = false
     $scope.$parent.param.headerPrice.title = "绑定用户"
     getBeautyQRCode.get({
-        shopId: "20180514102629597",
-        userId: "000b0a59-f75e-4bc9-95af-d5751431298b"
+        shopId: $stateParams.shopId,
+        userId: $stateParams.userId
     }, function (data) {
         $scope.qrCodeSrc = data.responseData
     })
 
     $scope.queryInfo = function () {
         getUserScanInfo.get({
-            shopId: "20180514102629597",
-            sysUserId: "000b0a59-f75e-4bc9-95af-d5751431298b"
+            shopId: $stateParams.shopId,
+            sysUserId: $stateParams.userId,
         }, function (data) {
             if(data.result == "0x00001"){
                 alert("SUCCESS")
