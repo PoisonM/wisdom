906f8c4301c632c96b3c229b941f55cb5e742b93
glass <m17610113689@163.com>
2018-05-15 20:50:43 +0800

    员工分析与档案

--- project/wisdom-project/src/main/resources/static/boss/js/controllers/archives/recordCashierCtrl.js
+++ project/wisdom-project/src/main/resources/static/boss/js/controllers/archives/recordCashierCtrl.js
@@ -1,8 +1,8 @@
 angular.module('controllers',[]).controller('recordCashierCtrl',
-    ['$scope','$rootScope','$stateParams','$state','$ionicLoading','$ionicPopup',
-        function ($scope,$rootScope,$stateParams,$state,$ionicLoading,$ionicPopup) {
+    ['$scope','$rootScope','$stateParams','$state','$ionicLoading','$ionicPopup','Consumes','Global',
+        function ($scope,$rootScope,$stateParams,$state,$ionicLoading,$ionicPopup,Consumes,Global) {
             $rootScope.title = "收银记录";
-            $scope.showAlert = function() {
+           /* $scope.showAlert = function() {
                 var alertPopup = $ionicPopup.alert({
                     title:"提示",
                     template: '<p class="fs17 col333 center">起始时间不能大于结束时间</p>'
@@ -11,9 +11,22 @@ angular.module('controllers',[]).controller('recordCashierCtrl',
                 alertPopup.then(function(res) {
                     console.log('Thank you for not eating my delicious ice cream cone');
                 });
-            };
-            $scope.showAlert();
+            };*/
+
             $scope.detailsOfCashierGo=function () {
                 $state.go("detailsOfCashier")
             }
+
+            $scope.userConsumeRequest = {
+                consumeType:'2',
+                goodType:"6",
+                pageSize:1000,
+                shopUserId:"11"
+
+            }
+            Consumes.save($scope.userConsumeRequest,function(data){
+                if(data.result==Global.SUCCESS&&data.responseData!=null){
+                    $scope.recordCashier =data.responseData
+                }
+            })
         }]);
