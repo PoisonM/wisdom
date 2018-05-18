0917ace72b9060a218adae2ca1f35e30feeadb74
tangyanchun <tangyanchun@qq.com>
2018-05-18 18:03:38 +0800

    shareHome.ctrl.js

--- project/wisdom-project/src/main/resources/static/customer/js/controllers/business/shareHomeCtrl.js
+++ project/wisdom-project/src/main/resources/static/customer/js/controllers/business/shareHomeCtrl.js
@@ -1,58 +1,64 @@
 angular.module('controllers',[]).controller('shareHomeCtrl',
-    ['$scope','$rootScope','$stateParams','$state','GetUserInfoByOpenId','Global','$ionicPopup',
-        function ($scope,$rootScope,$stateParams,$state,GetUserInfoByOpenId,Global,$ionicPopup) {
+    ['$scope','$rootScope','$stateParams','$state','GetUserInfoByOpenId','Global','$ionicPopup','IsLogin',
+        function ($scope,$rootScope,$stateParams,$state,GetUserInfoByOpenId,Global,$ionicPopup,IsLogin) {
 
             $rootScope.title = "分享赚钱";
 
             $scope.goSharePage = function()
             {
-                GetUserInfoByOpenId.get(function(data){
+                IsLogin.save(function(data){
+                    if(data.responseData=="failure"){
+                        $state.go("login");
+                    }else{
+                        GetUserInfoByOpenId.get(function(data){
 
-                    if(data.result==Global.SUCCESS)
-                    {
-                        if(data.responseData.userType=="business-A-1"||data.responseData.userType=="business-B-1"){
-                            $state.go("sharePage");
-                        }else
-                        {
-                            var alertPopup = $ionicPopup.alert({
-                                template: '<span style="font-size: 0.3rem;color: #333333;"> 立做美享店主，坐拥75%返利</span>',
-                                buttons: [
-                                    {
-                                        text: '不用啦'
-                                    },
-                                    {
-                                        onTap: function() {
-                                            $state.go("shopHome")
-                                        },
-                                        text: '成为店主',
-                                        type: 'button-calm'
-                                    }
-                                ]
+                            if(data.result==Global.SUCCESS)
+                            {
+                                if(data.responseData.userType=="business-A-1"||data.responseData.userType=="business-B-1"){
+                                    $state.go("sharePage");
+                                }else
+                                {
+                                    var alertPopup = $ionicPopup.alert({
+                                        template: '<span style="font-size: 0.3rem;color: #333333;"> 立做美享店主，坐拥75%返利</span>',
+                                        buttons: [
+                                            {
+                                                text: '不用啦'
+                                            },
+                                            {
+                                                onTap: function() {
+                                                    $state.go("shopHome")
+                                                },
+                                                text: '成为店主',
+                                                type: 'button-calm'
+                                            }
+                                        ]
 
-                            });
-                            // alert("亲，请先升级为我们的铂金店主或者钻石店主");
-                        }
-                    }
-                    else{
-                        var alertPopup = $ionicPopup.alert({
-                            template: '<span style="font-size: 0.3rem;color: #333333;">立做美享店主，坐拥75%返利</span>',
-                            buttons: [
-                                {
-                                    text: '不用啦'
-                                },
-                                {
-                                    onTap: function() {
-                                        $state.go("shopHome")
-                                    },
-                                    text: '成为店主',
-                                    type: 'button-calm'
+                                    });
                                 }
-                            ]
+                            }
+                            else{
+                                var alertPopup = $ionicPopup.alert({
+                                    template: '<span style="font-size: 0.3rem;color: #333333;">立做美享店主，坐拥75%返利</span>',
+                                    buttons: [
+                                        {
+                                            text: '不用啦'
+                                        },
+                                        {
+                                            onTap: function() {
+                                                $state.go("shopHome")
+                                            },
+                                            text: '成为店主',
+                                            type: 'button-calm'
+                                        }
+                                    ]
 
-                        });
-                        // alert("亲，请先升级为我们的铂金店主或者钻石店主");
+                                });
+                                // alert("亲，请先升级为我们的铂金店主或者钻石店主");
+                            }
+                        })
                     }
-                })
+                });
+
 
             }
 }])
