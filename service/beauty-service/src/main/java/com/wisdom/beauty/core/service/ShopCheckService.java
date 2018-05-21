0e45ee2df89f18c29212c6dcffffa3e84cb442b8
zh <zh>
2018-05-21 11:29:18 +0800

    ADD CODE

--- service/beauty-service/src/main/java/com/wisdom/beauty/core/service/ShopCheckService.java
+++ service/beauty-service/src/main/java/com/wisdom/beauty/core/service/ShopCheckService.java
@@ -1,8 +1,10 @@
 package com.wisdom.beauty.core.service;
 
 import com.wisdom.beauty.api.dto.ShopCheckRecordDTO;
+import com.wisdom.beauty.api.responseDto.ShopStockResponseDTO;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zhanghuan on 2018/5/21.
@@ -16,5 +18,13 @@ public interface ShopCheckService {
     *@Date:2018/5/21 9:50
     */
 
-    List<ShopCheckRecordDTO> getProductCheckRecord(ShopCheckRecordDTO shopCheckRecordDTO);
+    List<ShopCheckRecordDTO> getProductCheckRecordList(ShopCheckRecordDTO shopCheckRecordDTO);
+    /**
+    *@Author:zhanghuan
+    *@Param:
+    *@Return:
+    *@Description: 盘点记录详情
+    *@Date:2018/5/21 10:10
+    */
+    Map<String ,Object> getProductCheckRecordDeatil(ShopCheckRecordDTO shopCheckRecordDTO);
 }
