33bc8b41ace76140d685174aca6ed89fb2017cf7
tangyanchun <tangyanchun@qq.com>
2018-05-21 10:59:02 +0800

    productBrandCtrl 路由

--- project/wisdom-project/src/main/resources/static/boss/js/controllers/Stock/productBrandCtrl.js
+++ project/wisdom-project/src/main/resources/static/boss/js/controllers/Stock/productBrandCtrl.js
@@ -2,8 +2,8 @@
  * Created by Administrator on 2018/5/6.
  */
 angular.module('controllers',[]).controller('productBrandCtrl',
-    ['$scope','$rootScope','$stateParams','$state',
-        function ($scope,$rootScope,$stateParams,$state) {
+    ['$scope','$rootScope','$stateParams','$state','SaveProductTypeInfo',
+        function ($scope,$rootScope,$stateParams,$state,SaveProductTypeInfo) {
 
             $rootScope.title = "产品品牌";
 
@@ -11,12 +11,16 @@ angular.module('controllers',[]).controller('productBrandCtrl',
                 $state.go("addSeries")
             };
             $scope.addBrandOneGo = function(){
-                $state.go("addBrandOne")
+                $state.go("productSetting");
+              /*  SaveProductTypeInfo.get({productTypeName:"",status:""},function (data) {
+                    console.log(data)
+                })*/
             };
             $scope.checkProduct=function () {
-                $state.go("addBrandOne")
+                $state.go("productSetting")
             };
             $scope.checkSeries=function () {
                 $state.go("addSeries")
-            }
+            };
+
         }]);
