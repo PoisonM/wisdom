901fb62a0dcb17b108a4821aa8ac6dba060e0d85
zhaodeliang <1975063953@qq.com>
2018-05-22 15:47:10 +0800

    老板信息

--- service/beauty-service/src/test/java/com/wisdom/service/impl/MineTest.java
+++ service/beauty-service/src/test/java/com/wisdom/service/impl/MineTest.java
@@ -1,6 +1,8 @@
 package com.wisdom.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wisdom.beauty.BeautyServiceApplication;
+import com.wisdom.beauty.api.extDto.ExtShopBossDTO;
 import com.wisdom.common.util.SpringUtil;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +19,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -50,6 +53,27 @@ public class MineTest {
     public void getShopClerkScheduleList() throws Exception {
 
         MvcResult result = mvc.perform(get("/mine/getCurrentLoginUserInfo"))
+                .andExpect(status().isOk())// 模拟向testRest发送get请求
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))// 预期返回值的媒体类型text/plain;charset=UTF-8
+                .andReturn();// 返回执行请求的结果
+
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    /**
+     * 查询某个点的排班信息
+     *
+     * @throws Exception
+     */
+    @Test
+    public void updateShopClerkScheduleList() throws Exception {
+
+        ExtShopBossDTO extShopBossDTO = new ExtShopBossDTO();
+        extShopBossDTO.setMobile("18810142926");
+        extShopBossDTO.setId("1");
+        String toJSONString = JSONObject.toJSONString(extShopBossDTO);
+        System.out.println(toJSONString);
+        MvcResult result = mvc.perform(post("/mine/updateBossInfo").contentType(MediaType.APPLICATION_JSON).content(toJSONString))
                 .andExpect(status().isOk())// 模拟向testRest发送get请求
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))// 预期返回值的媒体类型text/plain;charset=UTF-8
                 .andReturn();// 返回执行请求的结果
