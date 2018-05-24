46113b711e022d07153fb17e1758772413da392e
cjk18 <cus18@sina.com>
2018-05-24 13:02:59 +0800

    wisdom

--- project/wisdom-project/src/main/resources/static/boss/js/controllers/inventory/chooseWarehouseCtrl.js
+++ project/wisdom-project/src/main/resources/static/boss/js/controllers/inventory/chooseWarehouseCtrl.js
@@ -2,14 +2,13 @@ angular.module('controllers',[]).controller('chooseWarehouseCtrl',
     ['$scope','$rootScope','$stateParams','$state','$ionicLoading','GetBossShopList','Global',
         function ($scope,$rootScope,$stateParams,$state,$ionicLoading,GetBossShopList,Global) {
             $rootScope.title = "选择仓库";
-            GetBossShopList.get({
-
-            },function(data){
+            GetBossShopList.get({},function(data){
                 if(data.result==Global.SUCCESS&&data.responseData!=null) {
-                   $scope.chooseWarehouse = data.responseData
-
+                   $scope.chooseWarehouse = data.responseData;
+                   console.log(data.responseData);
                 }
             })
+
             $scope.funAreaGo = function(){
                 $state.go("funArea")
             }
--- project/wisdom-project/src/main/resources/static/boss/js/controllers/inventory/putInStorageCtrl.js
+++ project/wisdom-project/src/main/resources/static/boss/js/controllers/inventory/putInStorageCtrl.js
@@ -1,13 +1,21 @@
 angular.module('controllers', []).controller('putInStorageCtrl',
-    ['$scope', '$rootScope', '$stateParams', '$state', '$ionicLoading',
-        function ($scope, $rootScope, $stateParams, $state, $ionicLoading) {
+    ['$scope', '$rootScope', '$stateParams', '$state', '$ionicLoading','GetShopProductLevelInfo',
+        function ($scope, $rootScope, $stateParams, $state, $ionicLoading,GetShopProductLevelInfo) {
             $rootScope.title = "入库";
             $scope.param = {
                 flag: false,
                 type: 0, /*客装产品  易耗品*/
                 selType: 0, /*扫码入库  手动入库*/
-                ids: []/*入库产品*/
+                ids: [],/*入库产品*/
+                detailProductList:[],
+                searchProductList:[],
+                searchContent :""
             };
+
+            GetShopProductLevelInfo.get({},function(data){
+                $scope.param.detailProductList = data.responseData.detailProductList;
+            })
+
             $scope.changeBtn = function (type) {
                 $scope.param.type = type;
             };
@@ -40,12 +48,36 @@ angular.module('controllers', []).controller('putInStorageCtrl',
                     $scope.param.ids.push(domIndex);
                 }
             };
+
             $scope.inventoryRecordsPicsGo = function(){
                 $state.go("inventoryRecordsPics")
             };
+
             $scope.newLibraryGo = function(){
                 $state.go("newLibrary",{stockStyle:$scope.param.selType})
             }
 
+            $scope.search = function(){
+                $scope.param.searchProductList = [];
+                GetShopProductLevelInfo.get({},function(data){
+                    $scope.param.detailProductList = data.responseData.detailProductList;
+                    angular.forEach($scope.param.detailProductList,function (value,key) {
+                        if(value.productTypeOneName.indexOf($scope.param.searchContent)!=-1||
+                            value.productName.indexOf($scope.param.searchContent)!=-1||
+                            (value.productCode+"").indexOf($scope.param.searchContent)!=-1)
+                        {
+                            $scope.param.searchProductList.push(value);
+                        }
+                    })
+                    $scope.param.detailProductList = angular.copy($scope.param.searchProductList);
+                })
+            }
 
-        }])
+            $scope.clearSearch = function()
+            {
+                $scope.param.searchContent = "";
+                GetShopProductLevelInfo.get({},function(data){
+                    $scope.param.detailProductList = data.responseData.detailProductList;
+                })
+            }
+}])
