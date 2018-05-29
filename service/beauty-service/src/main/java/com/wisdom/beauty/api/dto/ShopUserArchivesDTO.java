afa8d62d01ae254902df7c2b162b7aa3a919a02f
cjk18 <cus18@sina.com>
2018-05-29 14:41:45 +0800

    wisdom

--- service/beauty-service/src/main/java/com/wisdom/beauty/api/dto/ShopUserArchivesDTO.java
+++ service/beauty-service/src/main/java/com/wisdom/beauty/api/dto/ShopUserArchivesDTO.java
@@ -81,6 +81,8 @@ public class ShopUserArchivesDTO extends BaseEntity implements Serializable {
     //
     private Date updateDate;
 
+    private String imageRul;
+
     private static final long serialVersionUID = 1L;
 
     public String getId() {
@@ -281,5 +283,13 @@ public class ShopUserArchivesDTO extends BaseEntity implements Serializable {
 
     public void setUpdateDate(Date updateDate) {
         this.updateDate = updateDate;
+    }
+
+    public void setImageRul(String imageRul) {
+        this.imageRul = imageRul;
+    }
+
+    public String getImageRul() {
+        return imageRul;
     }
 }
