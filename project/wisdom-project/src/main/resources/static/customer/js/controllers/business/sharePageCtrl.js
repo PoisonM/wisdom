42c3a6935ddb0e5d89544bd7ac4921bb1c4f3806
zhangchao <347280977@qq.com>
2018-05-30 19:32:24 +0800

    修复交易明细显示问题

--- project/wisdom-project/src/main/resources/static/customer/js/controllers/business/sharePageCtrl.js
+++ project/wisdom-project/src/main/resources/static/customer/js/controllers/business/sharePageCtrl.js
@@ -35,7 +35,7 @@ angular.module('controllers',[]).controller('sharePageCtrl',
                 $scope.param.ctx.font = '36px microsoft #FFF100';
                 $scope.param.ctx.fillText($scope.param.weixinShareInfo.userType,328,150);*/
                 //二维码
-                $scope.param.ctx.drawImage($scope.param.imgs.qrCode, $scope.param.canvas.width*0.42, $scope.param.canvas.height*0.78 , $scope.param.canvas.width*0.23, calcHeight($scope.param.imgs.qrCode, $scope.param.canvas.width*0.23));
+                $scope.param.ctx.drawImage($scope.param.imgs.qrCode, $scope.param.canvas.width*0.4, $scope.param.canvas.height*0.78 , $scope.param.canvas.width*0.23, calcHeight($scope.param.imgs.qrCode, $scope.param.canvas.width*0.23));
 
                 //获取base64格式的src
                 var imgSrc = $scope.param.canvas.toDataURL('image/jpg');
