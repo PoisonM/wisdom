a853a8d19cd0fe379272e1899885a3237596911d
zhaodeliang <1975063953@qq.com>
2018-06-07 09:42:20 +0800

    错误解决

--- project/wisdom-project/src/main/resources/static/customer/js/controllers/beauty/beautyAppointCtrl.js
+++ project/wisdom-project/src/main/resources/static/customer/js/controllers/beauty/beautyAppointCtrl.js
@@ -22,78 +22,14 @@ angular.module('controllers',[]).controller('beautyAppointCtrl',
 
             var appointTimeDate = function () {
                 $scope.param.timeDate = [
-                    [{index: '0', value: '00:00', status: '1'}, {index: '1', value: '00:30', status: '1'}, {
-                        index: '2',
-                        value: '01:00',
-                        status: '1'
-                    }, {index: '3', value: '01:30', status: '1'}, {
-                        index: '4',
-                        value: "02:00",
-                        status: '1'
-                    }, {index: '5', value: '02:30', status: '1'}],
-                    [{index: '6', value: '03:00', status: '1'}, {index: '7', value: '03:30', status: '1'}, {
-                        index: '8',
-                        value: '04:00',
-                        status: '1'
-                    }, {index: '9', value: '04:30', status: '1'}, {
-                        index: '10',
-                        value: "05:00",
-                        status: '1'
-                    }, {index: '11', value: '05:30', status: '1'}],
-                    [{index: '12', value: '06:00', status: '1'}, {
-                        index: '13',
-                        value: '06:30',
-                        status: '1'
-                    }, {index: '14', value: '07:00', status: '1'}, {
-                        index: '15',
-                        value: '07:30',
-                        status: '1'
-                    }, {index: '16', value: "08:00", status: '1'}, {index: '17', value: '08:30', status: '1'}],
-                    [{index: '18', value: '09:00', status: '1'}, {
-                        index: '19',
-                        value: '09:30',
-                        status: '1'
-                    }, {index: '20', value: '10:00', status: '1'}, {
-                        index: '21',
-                        value: '10:30',
-                        status: '1'
-                    }, {index: '22', value: "11:00", status: '1'}, {index: '23', value: '11:30', status: '1'}],
-                    [{index: '24', value: '12:00', status: '1'}, {
-                        index: '25',
-                        value: '12:30',
-                        status: '1'
-                    }, {index: '26', value: '13:00', status: '1'}, {
-                        index: '27',
-                        value: '13:30',
-                        status: '1'
-                    }, {index: '28', value: "14:00", status: '1'}, {index: '29', value: '14:30', status: '1'}],
-                    [{index: '30', value: '15:00', status: '1'}, {
-                        index: '31',
-                        value: '15:30',
-                        status: '1'
-                    }, {index: '32', value: '16:00', status: '1'}, {
-                        index: '33',
-                        value: '16:30',
-                        status: '1'
-                    }, {index: '34', value: "17:00", status: '1'}, {index: '35', value: '17:30', status: '1'}],
-                    [{index: '36', value: '18:00', status: '1'}, {
-                        index: '37',
-                        value: '18:30',
-                        status: '1'
-                    }, {index: '38', value: '19:00', status: '1'}, {
-                        index: '39',
-                        value: '19:30',
-                        status: '1'
-                    }, {index: '40', value: "20:00", status: '1'}, {index: '41', value: '20:30', status: '1'}],
-                    [{index: '42', value: '21:00', status: '1'}, {
-                        index: '43',
-                        value: '21:30',
-                        status: '1'
-                    }, {index: '44', value: '22:00', status: '1'}, {
-                        index: '45',
-                        value: '22:30',
-                        status: '1'
-                    }, {index: '46', value: "23:00", status: '1'}, {index: '47', value: '23:30', status: '1'}],
+                    [{index:'0',value:'00:00',status:'1'},{index:'1',value:'00:30',status:'1'},{index:'2',value:'01:00',status:'1'},{index:'3',value:'01:30',status:'1'},{index:'4',value:"02:00",status:'1'},{index:'5',value:'02:30',status:'1'}],
+                    [{index:'6',value:'03:00',status:'1'},{index:'7',value:'03:30',status:'1'},{index:'8',value:'04:00',status:'1'},{index:'9',value:'04:30',status:'1'},{index:'10',value:"05:00",status:'1'},{index:'11',value:'05:30',status:'1'}],
+                    [{index:'12',value:'06:00',status:'1'},{index:'13',value:'06:30',status:'1'},{index:'14',value:'07:00',status:'1'},{index:'15',value:'07:30',status:'1'},{index:'16',value:"08:00",status:'1'},{index:'17',value:'08:30',status:'1'}],
+                    [{index:'18',value:'09:00',status:'1'},{index:'19',value:'09:30',status:'1'},{index:'20',value:'10:00',status:'1'},{index:'21',value:'10:30',status:'1'},{index:'22',value:"11:00",status:'1'},{index:'23',value:'11:30',status:'1'}],
+                    [{index:'24',value:'12:00',status:'1'},{index:'25',value:'12:30',status:'1'},{index:'26',value:'13:00',status:'1'},{index:'27',value:'13:30',status:'1'},{index:'28',value:"14:00",status:'1'},{index:'29',value:'14:30',status:'1'}],
+                    [{index:'30',value:'15:00',status:'1'},{index:'31',value:'15:30',status:'1'},{index:'32',value:'16:00',status:'1'},{index:'33',value:'16:30',status:'1'},{index:'34',value:"17:00",status:'1'},{index:'35',value:'17:30',status:'1'}],
+                    [{index:'36',value:'18:00',status:'1'},{index:'37',value:'18:30',status:'1'},{index:'38',value:'19:00',status:'1'},{index:'39',value:'19:30',status:'1'},{index:'40',value:"20:00",status:'1'},{index:'41',value:'20:30',status:'1'}],
+                    [{index:'42',value:'21:00',status:'1'},{index:'43',value:'21:30',status:'1'},{index:'44',value:'22:00',status:'1'},{index:'45',value:'22:30',status:'1'},{index:'46',value:"23:00",status:'1'},{index:'47',value:'23:30',status:'1'}],
                 ]
             }
 
