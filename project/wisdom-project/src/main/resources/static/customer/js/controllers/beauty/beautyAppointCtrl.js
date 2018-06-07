505f669265e217adc080b910948bb57b826d78e5
zhaodeliang <1975063953@qq.com>
2018-06-07 10:18:06 +0800

    '...'可以换成其它文字

--- project/wisdom-project/src/main/resources/static/customer/js/controllers/beauty/beautyAppointCtrl.js
+++ project/wisdom-project/src/main/resources/static/customer/js/controllers/beauty/beautyAppointCtrl.js
@@ -240,3 +240,25 @@ angular.module('controllers',[]).controller('beautyAppointCtrl',
             }
 
 }])
+
+var beauty = angular.module('filter', []);
+beauty.filter('textLengthSet', function() {
+    return function(value, wordwise, max, tail) {
+        if (!value) {
+            return '';
+        }
+        max = parseInt(max, 10);
+        if (!max) return value;
+        if (value.length <= max){
+            return value;
+        }
+        value = value.substr(0, max);
+        if (wordwise) {
+            var lastspace = value.lastIndexOf(' ');
+            if (lastspace != -1) {
+                value = value.substr(0, lastspace);
+            }
+        }
+        return value + (tail || ' …');//'...'可以换成其它文字
+    };
+});
