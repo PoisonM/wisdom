5bfd5e356607ac8687165d16537aa2c665a062c2
cjk18 <cus18@sina.com>
2018-06-09 10:49:00 +0800

    wisdom

--- project/wisdom-project/src/main/resources/static/boss/js/controllers/employee/employeeMyselfCtrl.js
+++ project/wisdom-project/src/main/resources/static/boss/js/controllers/employee/employeeMyselfCtrl.js
@@ -7,14 +7,17 @@ angular.module('controllers',[]).controller('employeeMyselfCtrl',
         function ($scope,$rootScope,$stateParams,$state) {
 
             $rootScope.title = "我的";
+
             /*点击头像跳转*/
             $scope.modificationInformationGo=function () {
                 $state.go("employeeInformation")
             };
+
             /*点击我的门店跳转*/
             $scope.myStoreGo=function () {
                 $state.go("employeeMyStore")
             };
+
             /*点击设置*/
             $scope.setInformationGo=function () {
                 $state.go("employeeSet")
