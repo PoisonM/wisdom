c7c0cbe4d98bde4bc211d47095de372119804294
cjk18 <cus18@sina.com>
2018-06-10 14:30:41 +0800

    wisdom

--- project/wisdom-project/src/main/resources/static/boss/js/controllers/employee/employeeSetCtrl.js
+++ project/wisdom-project/src/main/resources/static/boss/js/controllers/employee/employeeSetCtrl.js
@@ -7,10 +7,12 @@ angular.module('controllers',[]).controller('employeeSetCtrl',
         function ($scope,$rootScope,$stateParams,$state) {
 
             $rootScope.title = "设置";
+
             /*点击关于我们跳转到关于我们页面*/
             $scope.aboutMineGo=function () {
                 $state.go("aboutMine")
             };
+
             /*点击退出登录*/
             $scope.exitLogon=function () {
                 $state.go("login")
