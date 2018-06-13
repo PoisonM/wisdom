ed0b9cd17bc76891f36c77937733660578de122d
zh <zh>
2018-06-13 15:19:35 +0800

    update

--- service/user-service/src/main/java/com/wisdom/user/service/impl/BossInfoServiceImpl.java
+++ service/user-service/src/main/java/com/wisdom/user/service/impl/BossInfoServiceImpl.java
@@ -1,8 +1,11 @@
 package com.wisdom.user.service.impl;
 
 import com.wisdom.common.dto.user.SysBossDTO;
+import com.wisdom.common.util.JedisUtils;
+import com.wisdom.common.util.StringUtils;
 import com.wisdom.user.mapper.SysBossMapper;
 import com.wisdom.user.service.BossInfoService;
+import com.wisdom.user.util.UserUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +30,9 @@ public class BossInfoServiceImpl implements BossInfoService{
         logger.info("更新老板信息传入参数={}", "sysBossDTO = [" + sysBossDTO + "]");
         int update = sysBossMapper.updateByPrimaryKeySelective(sysBossDTO);
         logger.info("更新老板信息更新结果={}", update > 0 ? "成功" : "失败");
+        //更新redis中boss的信息
+        String token=UserUtils.getToken();
+        //JedisUtils.del(token);
         return update;
     }
 }
