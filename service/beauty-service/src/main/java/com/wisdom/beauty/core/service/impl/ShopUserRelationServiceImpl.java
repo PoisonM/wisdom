4a975c7cf422b01e650e856a91b979110d375bae
201 <790785836@qq.com>
2018-06-15 19:51:46 +0800

    sss

--- service/beauty-service/src/main/java/com/wisdom/beauty/core/service/impl/ShopUserRelationServiceImpl.java
+++ service/beauty-service/src/main/java/com/wisdom/beauty/core/service/impl/ShopUserRelationServiceImpl.java
@@ -170,9 +170,10 @@ public class ShopUserRelationServiceImpl implements ShopUserRelationService {
                     logger.info("此用户没有绑定过店铺，创建绑定关系");
                     shopUserRelationDTO.setStatus(CommonCodeEnum.BINDING.getCode());
                     shopUserRelationService.saveUserShopRelation(shopUserRelationDTO);
-                    JedisUtils.set(shopId+"_"+userId,"binding", ConfigConstant.logintokenPeriod);
                 }
+                JedisUtils.set(shopId+"_"+userId,"alreadyBind", ConfigConstant.logintokenPeriod);
             }
+
         }
         ResponseDTO<String> responseDTO = new ResponseDTO<>();
         responseDTO.setResult(StatusConstant.SUCCESS);
