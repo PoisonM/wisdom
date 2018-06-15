5c9e8cbefc2aac1467e6babacee3b70dc008f8f5
taoshiwen <624301656@qq.com>
2018-06-15 15:44:47 +0800

    接口调试

--- project/wisdom-project/src/main/resources/static/reception/itemList/addRecord/bindMember.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/addRecord/bindMember.js
@@ -1,12 +1,13 @@
-PADWeb.controller('bindMemberCtrl', function ($scope,$stateParams, $state, getBeautyQRCode, getUserScanInfo) {
+PADWeb.controller('bindMemberCtrl', function ($scope,$rootScope,$stateParams, $state, getBeautyQRCode, getUserScanInfo) {
     console.log($scope);
     /*-------------------------------------------定义头部/左边信息--------------------------------*/
-    $scope.$parent.mainSwitch.headerPriceListAllFlag = true
-    $scope.$parent.mainSwitch.headerCashAllFlag = false
-    $scope.$parent.mainSwitch.headerReservationAllFlag = false
-    $scope.$parent.mainSwitch.headerPriceListBlackFlag = false
-    $scope.$parent.mainSwitch.footerBoxFlag = false
-    $scope.$parent.param.headerPrice.title = "绑定用户"
+    $scope.$parent.mainSwitch.headerPriceListAllFlag = true;
+    $scope.$parent.mainSwitch.headerCashAllFlag = false;
+    $scope.$parent.mainSwitch.headerReservationAllFlag = false;
+    $scope.$parent.mainSwitch.headerPriceListBlackFlag = false;
+    $scope.$parent.mainSwitch.footerBoxFlag = false;
+    $scope.$parent.param.headerPrice.title = "绑定用户";
+    $scope.$parent.param.headerPrice.saveContent = ""
     getBeautyQRCode.get({
         shopId: $stateParams.shopId,
         userId: $stateParams.userId
@@ -21,7 +22,7 @@ PADWeb.controller('bindMemberCtrl', function ($scope,$stateParams, $state, getBe
         }, function (data) {
             if(data.result == "0x00001"){
                 alert("SUCCESS")
-                clearInterval($scope.timeInfo)
+                clearInterval($rootScope.timeInfo)
                 $state.go("pad-web.left_nav.personalFile")
             }
             if(data.result == "0x00002"){
@@ -30,18 +31,20 @@ PADWeb.controller('bindMemberCtrl', function ($scope,$stateParams, $state, getBe
             if(data.result == "alreadyBind"){
                 alert("已绑定")
                 $state.go("pad-web.left_nav.personalFile")
-                clearInterval($scope.timeInfo)
+                clearInterval($rootScope.timeInfo)
             }
         })
     }
 
-    $scope.timeInfo = setInterval(function () {
+    $rootScope.timeInfo = setInterval(function () {
         $scope.queryInfo()
     },3000)
     /*去其他页面清除定时器*/
     $scope.priceListBlackFn = function () {
         $state.go("pad-web.left_nav.personalFile")
     }
-
-
+    $scope.$parent.priceListBlackFn = function () {
+        window.history.go(-1)
+        clearInterval($rootScope.timeInfo)
+    }
 });
