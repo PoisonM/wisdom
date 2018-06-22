9f64d9e16410592a723702c17222b1d3fbd85a86
王保伟 <565871712@qq.com>
2018-06-22 15:31:04 +0800

    今日收银

--- project/wisdom-project/src/main/resources/static/reception/itemList/userInfo/rechargeRecord.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/userInfo/rechargeRecord.js
@@ -3,6 +3,7 @@ PADWeb.controller('rechargeRecordCtrl', function($scope,$state,$stateParams, ngD
     $scope.$parent.$parent.param.headerCash.title="充值记录"
     $scope.$parent.$parent.param.headerCash.backContent = "今日收银记录"
     $scope.$parent.$parent.mainSwitch.headerCashFlag.headerCashRightFlag.leftFlag = true
+    $scope.$parent.mainSwitch.headerCashFlag.headerCashRightFlag.rightFlag = false
 
     //consumeType 0：充值 1：消费 2、还欠款 3、退款
     Consumes.save({
--- project/wisdom-project/src/main/resources/static/reception/itemList/userInfo/stampCardRecord.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/userInfo/stampCardRecord.js
@@ -3,7 +3,7 @@ PADWeb.controller('stampCardRecordCtrl', function($scope,$state, $stateParams, n
     $scope.$parent.$parent.param.headerCash.title="划卡记录"
     $scope.$parent.$parent.param.headerCash.backContent = "今日收银记录"
     $scope.$parent.$parent.mainSwitch.headerCashFlag.headerCashRightFlag.leftFlag = true
-    console.log('划卡记录');
+    $scope.$parent.mainSwitch.headerCashFlag.headerCashRightFlag.rightFlag = false
 
     //consumeType 0：充值 1：消费 2、还欠款 3、退款
     Consumes.save({
