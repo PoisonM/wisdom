8a2d42fbf4a33124dfdfdc8136b8cc59a7ce425b
zhaodeliang <1975063953@qq.com>
2018-06-24 14:28:33 +0800

    卡项目过期之后不能使用

--- project/wisdom-project/src/main/resources/static/reception/itemList/cashier/personalFileCtrl.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/cashier/personalFileCtrl.js
@@ -98,7 +98,11 @@ PADWeb.controller('personalFileCtrl', function($scope,$rootScope,$stateParams, $
     }
     // $scope.tabclick(0);
 
-    $scope.goConsumptionList = function(type, id) {
+    $scope.goConsumptionList = function(type, id,expirationDate) {
+        if(expirationDate<$rootScope.currentTime){
+            alert("对不起，你的卡项已过期^_^");
+            return false;
+        }
         $state.go('pad-web.left_nav.drawCardConsumption', { type: type, id: id });
     };
 
