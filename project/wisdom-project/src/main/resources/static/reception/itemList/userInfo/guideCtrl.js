65adeb34cfbcee4ef90a8eaa25fbde7093f83459
王保伟 <565871712@qq.com>
2018-06-24 15:26:56 +0800

    我的 页面样式问题

--- project/wisdom-project/src/main/resources/static/reception/itemList/userInfo/guideCtrl.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/userInfo/guideCtrl.js
@@ -1,3 +1,4 @@
 PADWeb.controller('guideCtrl', function($scope,$state, $stateParams,SuggestionDetail) {
-
+    $scope.$parent.mainSwitch.headerCashFlag.headerCashRightFlag.leftBackFlag = false
+    $scope.$parent.mainSwitch.headerCashFlag.headerCashRightFlag.rightFlag = false
 })
