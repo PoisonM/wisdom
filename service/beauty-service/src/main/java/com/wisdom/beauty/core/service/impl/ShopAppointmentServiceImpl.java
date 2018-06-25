cf06df6d410a41673188c367c3169aee62f5e047
王保伟 <565871712@qq.com>
2018-06-25 14:54:01 +0800

    更新预约信息时候更新redis

--- service/beauty-service/src/main/java/com/wisdom/beauty/core/service/impl/ShopAppointmentServiceImpl.java
+++ service/beauty-service/src/main/java/com/wisdom/beauty/core/service/impl/ShopAppointmentServiceImpl.java
@@ -214,6 +214,7 @@ public class ShopAppointmentServiceImpl implements ShopAppointmentService {
         if (shopAppointServiceDTO == null || StringUtils.isBlank(shopAppointServiceDTO.getId())) {
             return 0;
         }
+        redisUtils.saveShopAppointInfoToRedis(shopAppointServiceDTO);
         return shopAppointServiceMapper.updateByPrimaryKeySelective(shopAppointServiceDTO);
     }
 
