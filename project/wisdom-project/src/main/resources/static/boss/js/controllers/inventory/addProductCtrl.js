a601a464736584a4d399b5b6177c58b3ef48ae79
zh <zh>
2018-06-27 13:40:01 +0800

    fix bug

--- project/wisdom-project/src/main/resources/static/boss/js/controllers/inventory/addProductCtrl.js
+++ project/wisdom-project/src/main/resources/static/boss/js/controllers/inventory/addProductCtrl.js
@@ -228,7 +228,7 @@ angular.module('controllers',[]).controller('addProductCtrl',
                 }else{
                     $rootScope.settingAddsome.product.status = '1';
                 }
-                if($rootScope.settingAddsome.product.productTypeOneName == ""||$rootScope.settingAddsome.product.productTypeTwoName ==""||$rootScope.settingAddsome.product.productName ==""||$rootScope.settingAddsome.product.marketPrice ==""||$rootScope.settingAddsome.product.discountPrice ==""||$rootScope.settingAddsome.product.productSpec ==""||$rootScope.settingAddsome.product.productUnit ==""||$rootScope.settingAddsome.product.effectDate ==""||$rootScope.settingAddsome.product.qualityPeriod ==""||$rootScope.settingAddsome.product.productWarningDay ==""||$rootScope.settingAddsome.product.productWarningNum ==""){
+                if($rootScope.settingAddsome.product.productTypeOneName == ""||$rootScope.settingAddsome.product.productTypeTwoName ==""||$rootScope.settingAddsome.product.productName ==""||$rootScope.settingAddsome.product.marketPrice ==""||$rootScope.settingAddsome.product.initialPrice ==""||$rootScope.settingAddsome.product.productSpec ==""||$rootScope.settingAddsome.product.productUnit ==""||$rootScope.settingAddsome.product.effectDate ==""||$rootScope.settingAddsome.product.qualityPeriod ==""||$rootScope.settingAddsome.product.productWarningDay ==""||$rootScope.settingAddsome.product.productWarningNum ==""){
                     alert('信息不完全')
                     return
                 }
