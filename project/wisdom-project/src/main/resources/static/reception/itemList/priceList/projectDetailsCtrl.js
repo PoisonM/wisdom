b94f46b0fdf3e594f75989adbda374135c1eaeb2
taoshiwen <624301656@qq.com>
2018-06-30 16:41:46 +0800

    价目表bug修改

--- project/wisdom-project/src/main/resources/static/reception/itemList/priceList/projectDetailsCtrl.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/priceList/projectDetailsCtrl.js
@@ -15,17 +15,17 @@ PADWeb.controller("projectDetailsCtrl", function($scope, $state, $stateParams,Pr
     /*打开收银头部/档案头部/我的头部*/
     $scope.flagFn(true);
     $scope.$parent.mainSwitch.footerBoxFlag=false;
-    var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 3,
-        loop: true,
-        autoplayDisableOnInteraction : false,
-        autoplay:1500,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-    });
-
+    /*轮播图*/
+    $scope.timeer = setInterval(function () {
+        if($(".swiper-slide").length!=0){
+            clearInterval($scope.timeer)
+            var mySwiper = new Swiper('.swiper-container',{
+                slidesPerView : 3,
+                slidesPerGroup : 1,
+                spaceBetween : 20,
+            })
+        }
+    },100)
     ProjectInfo.get({id:$stateParams.id},function (data) {
         if(data.result == '0x00001'){
             $scope.projectInformation = data.responseData;
