cb26504bc3588468b469cac3c8d2b4743da3596f
zhaodeliang <1975063953@qq.com>
2018-06-30 17:10:56 +0800

    退出登录

--- project/wisdom-project/src/main/resources/static/customer/js/controllers/beauty/expenseCardRecordCtrl.js
+++ project/wisdom-project/src/main/resources/static/customer/js/controllers/beauty/expenseCardRecordCtrl.js
@@ -11,7 +11,7 @@ angular.module('controllers',[]).controller('expenseCardRecordCtrl',
                 expenseCardDetailData :'true'
             }
 
-            GetProjectConsumes.save({consumeType:'1',goodsType:'6',
+            GetProjectConsumes.save({consumeType:'1',goodsType:'5',
                 pageSize:$scope.param.pageSize,sysUserId:$rootScope.shopAppointInfo.shopUserInfo.id},function (data) {
                 if(data.result==Global.SUCCESS)
                 {
