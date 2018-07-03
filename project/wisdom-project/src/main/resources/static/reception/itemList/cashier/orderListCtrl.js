4e9c72541546d87c1c343a44abecb6083b8319d0
taoshiwen <624301656@qq.com>
2018-07-03 16:30:42 +0800

    bug修改

--- project/wisdom-project/src/main/resources/static/reception/itemList/cashier/orderListCtrl.js
+++ project/wisdom-project/src/main/resources/static/reception/itemList/cashier/orderListCtrl.js
@@ -3,7 +3,7 @@ PADWeb.controller('orderListCtrl', function($scope, $stateParams, $state, ngDial
     $scope.$parent.$parent.param.top_bottomSelect = "shouyin";
     // $scope.$parent.$parent.param.headerCash.leftContent = "档案(9010)";
     $scope.$parent.$parent.param.headerCash.leftAddContent = "添加档案";
-    $scope.$parent.$parent.param.headerCash.backContent = "消费";
+    $scope.$parent.$parent.param.headerCash.backContent = "返回";
     $scope.$parent.$parent.param.headerCash.leftTip = "保存";
     $scope.$parent.$parent.param.headerCash.title = "消费"
     $scope.$parent.$parent.mainSwitch.headerCashFlag.headerCashRightFlag.leftFlag = true;
