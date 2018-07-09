05e049bb54b06d1e9a1a69cafb131ec33aa308f8
tangyanchun <tangyanchun@qq.com>
2018-07-09 15:35:39 +0800

    用户端加菊花转动效果

--- project/wisdom-project/src/main/resources/static/customer/js/controllers/beauty/projectCardDetailCtrl.js
+++ project/wisdom-project/src/main/resources/static/customer/js/controllers/beauty/projectCardDetailCtrl.js
@@ -2,25 +2,32 @@
  * Created by Administrator on 2017/12/15.
  */
 angular.module('controllers',[]).controller('projectCardDetailCtrl',
-    ['$scope','$rootScope','$stateParams','$state','GetProjectCardConsumeByFlowId','Global',
-        function ($scope,$rootScope,$stateParams,$state,GetProjectCardConsumeByFlowId,Global) {
+    ['$scope','$rootScope','$stateParams','$state','GetProjectCardConsumeByFlowId','Global','$ionicLoading',
+        function ($scope,$rootScope,$stateParams,$state,GetProjectCardConsumeByFlowId,Global,$ionicLoading) {
+            $scope.$on('$ionicView.enter', function() {
+                $ionicLoading.show({
+                    content: 'Loading',
+                    animation: 'fade-in',
+                    showBackdrop: true,
+                    maxWidth: 200,
+                    showDelay: 0
+                });
+                $scope.param = {
+                    pageSize: 5000,
+                    projectCardConsumes: [],
+                    projectCardDetailData: "true"
+                };
 
-    $scope.param = {
-        pageSize:5000,
-        projectCardConsumes : [],
-        projectCardDetailData : "true"
-    }
+                GetProjectCardConsumeByFlowId.get({flowId: $stateParams.projectId, consumeType: "1"}, function (data) {
+                    $ionicLoading.hide();
+                    if (data.result == Global.SUCCESS) {
+                        if (data.responseData != null) {
+                            $scope.param.projectCardConsumes = data.responseData;
+                        } else {
+                            $scope.param.projectCardDetailData = "false";
+                        }
+                    }
+                })
+            })                
 
-    GetProjectCardConsumeByFlowId.get({flowId:$stateParams.projectId,consumeType:"1"},function (data) {
-        if(data.result==Global.SUCCESS)
-        {
-            if(data.responseData!=null)
-            {
-                $scope.param.projectCardConsumes = data.responseData;
-            }else {
-                $scope.param.projectCardDetailData = "false";
-            }
-        }
-    })
-
-}])
+}]);
--- project/wisdom-project/src/main/resources/static/customer/js/controllers/beauty/projectCardListCtrl.js
+++ project/wisdom-project/src/main/resources/static/customer/js/controllers/beauty/projectCardListCtrl.js
@@ -2,23 +2,34 @@
  * Created by Administrator on 2017/12/15.
  */
 angular.module('controllers',[]).controller('projectCardListCtrl',
-    ['$scope','$rootScope','$stateParams','$state','GetUserRechargeCardList','GetUserCourseProjectList','Global',
-        function ($scope,$rootScope,$stateParams,$state,GetUserRechargeCardList,GetUserCourseProjectList,Global) {
+    ['$scope','$rootScope','$stateParams','$state','GetUserRechargeCardList','GetUserCourseProjectList','Global','$ionicLoading',
+        function ($scope,$rootScope,$stateParams,$state,GetUserRechargeCardList,GetUserCourseProjectList,Global,$ionicLoading) {
+            $scope.$on('$ionicView.enter', function() {
+                $ionicLoading.show({
+                    content: 'Loading',
+                    animation: 'fade-in',
+                    showBackdrop: true,
+                    maxWidth: 200,
+                    showDelay: 0
+                });
+                $scope.param = {
+                    projectCardList: []
+                };
 
-            $scope.param = {
-                projectCardList : []
-            }
-
-            GetUserCourseProjectList.get({sysUserId:$rootScope.shopAppointInfo.shopUserInfo.id,cardStyle:'1'},function (data) {
-                if(data.result==Global.SUCCESS)
-                {
-                    $scope.param.projectCardList = data.responseData;
-                    console.log($scope.param.projectCardList);
-                }
+                GetUserCourseProjectList.get({
+                    sysUserId: $rootScope.shopAppointInfo.shopUserInfo.id,
+                    cardStyle: '1'
+                }, function (data) {
+                    $ionicLoading.hide();
+                    if (data.result == Global.SUCCESS) {
+                        $scope.param.projectCardList = data.responseData;
+                        console.log($scope.param.projectCardList);
+                    }
+                });
             })
+                $scope.chooseProjectCard = function (projectId) {
+                    $state.go("projectCardDetail", {projectId: projectId})
+                }
 
-            $scope.chooseProjectCard = function(projectId){
-                $state.go("projectCardDetail",{projectId:projectId})
-            }
 
-}])
+}]);
