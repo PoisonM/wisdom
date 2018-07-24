ded5f6ff022b42b17bafff99175e42d6b778fd1b
zhaodeliang <1975063953@qq.com>
2018-07-24 13:50:13 +0800

    mongo测试

--- service/beauty-service/src/test/java/com/wisdom/service/impl/MongonTest.java
+++ service/beauty-service/src/test/java/com/wisdom/service/impl/MongonTest.java
@@ -2,16 +2,13 @@ package com.wisdom.service.impl;
 
 import com.wisdom.beauty.BeautyServiceApplication;
 import com.wisdom.beauty.api.extDto.ImageUrl;
+import com.wisdom.beauty.core.redis.MongoUtils;
+import org.bson.types.ObjectId;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.mapping.Language;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -28,17 +25,21 @@ public class MongonTest {
 
     @Autowired
     private MongoTemplate mongoTemplate;
-    @Test
-   public void  testMongoing(){
-        //条件查询1，多条件is("值")后面可以加and("字段2").is("值2")
-/*       Query query=new Query(Criteria.where("_id").is("1"));
-        mongoTemplate.find(query, Object.class);*/
-            List<String> list=new ArrayList<>();
-            list.add("dfsfdffad");
 
-          Query query = new Query();
-          query.addCriteria(Criteria.where("imageId").is("dc7e6373c8264fc1a5833ed5ac39e4c39999999"));
-          Update update = Update.update("url", list);
-          mongoTemplate.updateFirst(query, update, ImageUrl.class);
-        }
+    @Autowired
+    private MongoUtils mongoUtils;
+
+    @Test
+   public void  mongoUtilsTest(){
+        String _id="5b02627fa34ec30da4988d8c";
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("3");
+        list.add("4");
+        ImageUrl imageUrl = new ImageUrl();
+        imageUrl.setUrl(list);
+
+        imageUrl.set_id(new ObjectId(_id));
+        mongoTemplate.save(imageUrl, "imageUrl");
+    }
 }
