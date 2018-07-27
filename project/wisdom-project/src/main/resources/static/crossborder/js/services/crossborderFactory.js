5d93a39ee6b7de75e84a1d225d21c08c8b7fe753
taoshiwen <624301656@qq.com>
2018-07-27 15:49:49 +0800

    跨境商城

--- project/wisdom-project/src/main/resources/static/crossborder/js/services/crossborderFactory.js
+++ project/wisdom-project/src/main/resources/static/crossborder/js/services/crossborderFactory.js
@@ -1,6 +1,6 @@
 
 
-var product ='/crossBorder/order/';
+var product ='/business/order/';
 // var product ='http://192.168.1.197/business/crossBorder/order/';
 
 define(['appCrossborder'], function (app) {
