73760d12598990d676c28a97c3c83edf5557982e
taoshiwen <624301656@qq.com>
2018-07-30 15:45:42 +0800

    跨境商城-弹窗

--- project/wisdom-project/src/main/resources/static/crossborder/js/controllers/orderSubmitCtrl.js
+++ project/wisdom-project/src/main/resources/static/crossborder/js/controllers/orderSubmitCtrl.js
@@ -2,5 +2,5 @@ angular.module('controllers',[]).controller('orderSubmitCtrl',
     ['$scope','$interval','$rootScope','$stateParams','$state','Global','$timeout',
         function ($scope,$interval,$rootScope,$stateParams,$state,Global,$timeout) {
             console.log("orderSubmitCtrl")
-
+            $scope.authentication_flag = false//弹窗
         }]);
