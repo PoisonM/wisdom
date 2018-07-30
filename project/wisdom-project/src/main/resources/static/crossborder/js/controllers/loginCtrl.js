c058d4a139e0030a2656023c5020118cd077b16a
taoshiwen <624301656@qq.com>
2018-07-30 14:50:19 +0800

    跨境商城

---
+++ project/wisdom-project/src/main/resources/static/crossborder/js/controllers/loginCtrl.js
@@ -0,0 +1,6 @@
+angular.module('controllers',[]).controller('loginCtrl',
+    ['$scope','$interval','$rootScope','$stateParams','$state','Global','$timeout',
+        function ($scope,$interval,$rootScope,$stateParams,$state,Global,$timeout) {
+
+
+        }]);
