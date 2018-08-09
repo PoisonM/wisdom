94ce91e8033d6fbdb250914ae13acd4221bd13bf
cp11 <cp111>
2018-08-09 18:24:26 +0800

    样式

--- service/business-service/src/main/java/com/wisdom/business/service/product/ProductService.java
+++ service/business-service/src/main/java/com/wisdom/business/service/product/ProductService.java
@@ -87,7 +87,7 @@ public class ProductService {
             OfflineProductDTO offlineProductDTO = mongoTemplate.findOne(query, OfflineProductDTO.class,"offlineProduct");
             if(null != offlineProductDTO){
                 offlineProductDTO.setNowTime(DateUtils.formatDateTime(new Date()));
-                offlineProductDTO.setProductSalesVolume(Integer.parseInt(sellNum)*8+"");
+                offlineProductDTO.setProductSalesVolume((Integer.parseInt(sellNum)*8+Integer.parseInt(offlineProductDTO.getProductSalesVolume())+""));
             }
             productDTO.setSellNum(sellNum);
             productDTO.setProductDetail(offlineProductDTO);
